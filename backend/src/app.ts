056e96b471455b8850c5ab03df6732b4b294d5f0
charan379 <37008444+charan379@users.noreply.github.com>
2024-03-02 13:36:06 +0530

    changes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,11 @@
+import { PrismaClient } from "@prisma/client";
 import express, { Express } from "express";
 
 
 // create express application instance
 const app: Express = express();
+
+const prisma = new PrismaClient();
 
 // export express app instance, so it can be used at 'server.ts' to start server
 export default app;
