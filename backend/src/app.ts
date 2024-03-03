2abf28d22eb53d2f45a8764c7f723e13fde2c37b
charan379 <37008444+charan379@users.noreply.github.com>
2024-03-03 22:20:05 +0530

    testing database working or not

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,30 @@
-import { PrismaClient } from "@prisma/client";
-import express, { Express } from "express";
+import express, { Express, NextFunction, Request, Response, } from "express";
+import prisma from "./lib/prisma";
+import { Prisma } from "@prisma/client";
 
 
 // create express application instance
 const app: Express = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-const prisma = new PrismaClient();
+app.post('/api/org', async (req: Request, res: Response, next: NextFunction) => {
+    const data: Prisma.OrganizationCreateInput = req.body;
+    console.log(data, req.body);
+    const org = await prisma.organization.create({
+        data: data
+    });
+    res.status(201).json(org);
+});
+
+app.get('/api/org/:id', async (req: Request, res: Response, next: NextFunction) => {
+    const org = await prisma.organization.findUnique({
+        where: {
+            id: Number.parseInt(req.params.id),
+    }
+    })
+    res.status(200).json(org);
+});
 
 // export express app instance, so it can be used at 'server.ts' to start server
 export default app;
