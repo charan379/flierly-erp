aa29d9db2ba596de7a635479241c9c1da415fc92
charan379 <37008444+charan379@users.noreply.github.com>
2024-03-08 21:17:07 +0530

    added includes

--- backend/src/modules/branch/branch.repository.ts
+++ backend/src/modules/branch/branch.repository.ts
@@ -1,36 +1,40 @@
 import { Prisma } from "@prisma/client";
 import prisma from "@/lib/prisma";
 
-async function save(newbranch: Prisma.BranchCreateInput) {
+async function save(newbranch: Prisma.BranchCreateInput, includes?: Prisma.BranchInclude) {
     return prisma.branch.create({
-        data: newbranch
+        data: newbranch,
+        include: includes
     });
 };
 
-async function findOneByName(name: string) {
+async function findOneByName(name: string, includes?: Prisma.BranchInclude) {
     return prisma.branch.findFirst({
         where: {
             name: { equals: name, mode: "insensitive" },
             isDeleted: false
         },
+        include: includes
     });
 };
 
-async function findOneByEmail(email: string) {
+async function findOneByEmail(email: string, includes?: Prisma.BranchInclude) {
     return prisma.branch.findFirst({
         where: {
             email: { equals: email, mode: "insensitive" },
             isDeleted: false
         },
+        include: includes
     });
 };
 
-async function findOneByPhone(phone: string) {
+async function findOneByPhone(phone: string, includes?: Prisma.BranchInclude) {
     return prisma.branch.findFirst({
         where: {
             phone: { equals: phone },
             isDeleted: false,
         },
+        include: includes
     });
 };
 
