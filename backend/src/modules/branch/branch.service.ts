4404ba34ec9bf6b477bb513d4feb319847935d74
charan379 <37008444+charan379@users.noreply.github.com>
2024-03-08 21:16:47 +0530

    branch services

--- backend/src/modules/branch/branch.service.ts
+++ backend/src/modules/branch/branch.service.ts
@@ -1,36 +1,34 @@
-// import { Prisma } from "@prisma/client";
-// import respository from "./branch.repository";
+import { Prisma } from "@prisma/client";
+import respository from "./branch.repository";
 
 
 
-// async function createBranch(branch: Branch): Promise<Branch> {
+async function createBranch(branch: Branch): Promise<Branch> {
 
-//     const data: Prisma.BranchCreateInput = {
-//         name: branch.name,
-//         email: branch.email,
-//         phone: branch.phone,
-//         alternatePhone: branch.alternatePhone,
-//         address: {
-//             create: undefined,
-//             connectOrCreate: undefined,
-//             connect: undefined
-//         },
-//         taxIdentity: {
-//             create: undefined,
-//             connectOrCreate: undefined,
-//             connect: undefined
-//         }
-//     }
-// }
+    const data: Prisma.BranchCreateInput = {
+        name: branch.name,
+        email: branch.email,
+        phone: branch.phone,
+        alternatePhone: branch.alternatePhone,
+        address: {
+            connectOrCreate: undefined,
+        },
+        taxIdentity: {
+            connectOrCreate: undefined,
+        }
+    }
 
-// async function existsByName(name: string): Promise<boolean> {
-//     return await respository.findOneByName(name) ? true : false;
-// }
+    return await respository.save(data) as Branch;
+}
 
-// async function existsByEmail(email: string): Promise<boolean> {
-//     return await respository.findOneByEmail(email) ? true : false;
-// }
+async function existsByName(name: string): Promise<boolean> {
+    return await respository.findOneByName(name) ? true : false;
+}
 
-// async function existsByPhone(phone: string): Promise<boolean> {
-//     return await respository.findOneByPhone(phone) ? true : false;
-// }
+async function existsByEmail(email: string): Promise<boolean> {
+    return await respository.findOneByEmail(email) ? true : false;
+}
+
+async function existsByPhone(phone: string): Promise<boolean> {
+    return await respository.findOneByPhone(phone) ? true : false;
+}
