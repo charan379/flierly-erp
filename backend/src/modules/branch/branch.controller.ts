24a9437b16169063cb298dee45627da53edc90be
charan379 <37008444+charan379@users.noreply.github.com>
2024-03-10 18:17:23 +0530

    new controllers added

--- backend/src/modules/branch/branch.controller.ts
+++ backend/src/modules/branch/branch.controller.ts
@@ -5,7 +5,7 @@ import branchService from "./branch.service";
 import Joi from "joi";
 
 
-export async function createBranch(req: Request, res: Response, next: NextFunction): Promise<void> {
+async function createBranch(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
         const data: Branch = await JoiSchemaValidator<Branch>(createBranchSchema, req.body, {}, "");
         const branch = await branchService.create(data);
@@ -15,7 +15,27 @@ export async function createBranch(req: Request, res: Response, next: NextFuncti
     }
 }
 
-export async function deleteBranch(req: Request, res: Response, next: NextFunction): Promise<void> {
+async function getBranchById(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+        const id = await JoiSchemaValidator<number>(Joi.number().integer().required(), req.params?.id, {}, "");
+        const branch = await branchService.fetchById(id);
+        res.status(200).json(branch);
+    } catch (error) {
+        next(error);
+    }
+}
+
+async function getStatusById(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+        const id = await JoiSchemaValidator<number>(Joi.number().integer().required(), req.params?.id, {}, "");
+        const branch = await branchService.fetchById(id);
+        res.status(200).json({ isActive: branch.isActive });
+    } catch (error) {
+        next(error);
+    }
+}
+
+async function deleteBranch(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
         const id = await JoiSchemaValidator<number>(Joi.number().integer().required(), req.params?.id, {}, "");
         const branch = await branchService.deleteById(id);
@@ -24,3 +44,34 @@ export async function deleteBranch(req: Request, res: Response, next: NextFuncti
         next(error);
     }
 }
+
+async function getExistenceByName(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+        const name = await JoiSchemaValidator<string>(Joi.string().required(), req.params?.name, {}, "");
+        res.status(200).json({ exists: await branchService.existsByName(name) });
+    } catch (error) {
+        next(error)
+    }
+}
+
+async function getExistenceByEmail(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+        const email = await JoiSchemaValidator<string>(Joi.string().email().required(), req.params?.email, {}, "");
+        res.status(200).json({ exists: await branchService.existsByEmail(email) });
+    } catch (error) {
+        next(error);
+    }
+};
+
+async function getExistenceByPhone(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+        const phone = await JoiSchemaValidator<string>(Joi.string().min(10).max(13).required(), req.params?.phone, {}, "");
+        res.status(200).json({ exists: await branchService.existsByPhone(phone) });
+    } catch (error) {
+        next(error);
+    }
+}
+
+const branchModuleController = { createBranch, getBranchById, getStatusById, deleteBranch, getExistenceByName, getExistenceByEmail, getExistenceByPhone };
+
+export default branchModuleController;
