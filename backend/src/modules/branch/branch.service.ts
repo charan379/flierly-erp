4a5d8a785544cb3462b4eef4f079c01bcab9ec62
charan379 <37008444+charan379@users.noreply.github.com>
2024-03-10 18:13:25 +0530

    added fetchById

--- backend/src/modules/branch/branch.service.ts
+++ backend/src/modules/branch/branch.service.ts
@@ -6,6 +6,10 @@ async function create(branch: Branch): Promise<Branch> {
     return await branchRespository.save(branch) as Branch;
 }
 
+async function fetchById(id: number): Promise<Branch> {
+    return await branchRespository.findOneById(id) as Branch;
+}
+
 async function existsByName(name: string): Promise<boolean> {
     return await branchRespository.findOneByName(name) ? true : false;
 }
@@ -22,6 +26,6 @@ async function deleteById(id: number): Promise<Branch> {
     return await branchRespository.deleteOneById(id) as Branch;
 };
 
-const branchService = { create, existsByName, existsByEmail, existsByPhone, deleteById };
+const branchService = { create, fetchById, existsByName, existsByEmail, existsByPhone, deleteById };
 
 export default branchService;
