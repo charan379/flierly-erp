e13a94fe6dbe83631594f31028263c3ca6f8cb32
charan379 <37008444+charan379@users.noreply.github.com>
2024-03-13 17:52:54 +0530

    added isActive field

--- backend/src/models/interfaces/account-subtype.interface.ts
+++ backend/src/models/interfaces/account-subtype.interface.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 export interface AccountSubtype {
     _id: mongoose.ObjectId,
     isDeleted: boolean,
+    isActive: boolean,
     code: string,
     name: string,
     parentTypeCode: string,
--- backend/src/models/interfaces/account-type.interface.ts
+++ backend/src/models/interfaces/account-type.interface.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 export interface AccountType {
     _id: mongoose.ObjectId,
     isDeleted: boolean,
+    isActive: boolean,
     code: string,
     name: string,
     createdAt: Date,
