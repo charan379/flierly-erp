ef8677e51de0411fe516d9810154d5bc06661ae1
charan379 <37008444+charan379@users.noreply.github.com>
2024-03-13 15:14:14 +0530

    added isActive, branchId, accountId

--- backend/src/models/interfaces/contact.interface.ts
+++ backend/src/models/interfaces/contact.interface.ts
@@ -1,9 +1,14 @@
+import mongoose from "mongoose";
+
 export interface Contact {
     isDeleted: boolean,
+    isActive: boolean,
     name: string,
     email: string,
     phone: string,
     alternatePhone: string,
+    branchId: mongoose.ObjectId,
+    accountId: mongoose.ObjectId,
     createdAt: Date,
     updatedAt: Date,
 }
