381c257a31124fb7076fe341a8ccb581070989c0
charan379 <37008444+charan379@users.noreply.github.com>
2024-03-13 17:53:21 +0530

    account type model

---
+++ backend/src/models/account-type.model.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import { AccountType } from "./interfaces/account-type.interface";
+
+const schema: mongoose.Schema<AccountType> = new mongoose.Schema<AccountType>(
+    {
+        isDeleted: {
+            type: Boolean,
+            default: false
+        },
+        isActive: {
+            type: Boolean,
+            default: true
+        },
+        code: {
+            type: String,
+            required: [true, "Account-type code is required."],
+            unique: true,
+            immutable: true
+        },
+        name: {
+            type: String,
+            required: [true, "Account type name is required."]
+        }
+    },
+    {
+        timestamps: true,
+        collection: "account-types"
+    }
+);
+
+const AccountTypeModel: mongoose.Model<AccountType> = mongoose.model<AccountType>('AccountType', schema);
+
+export default AccountTypeModel;
