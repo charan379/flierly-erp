a1aa7f7d8ec6c8d5729b81af2f12a27e790f2fc7
charan379 <37008444+charan379@users.noreply.github.com>
2024-03-19 14:34:51 +0530

    minor bug fixs

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -3,8 +3,8 @@ import { basename } from 'path';
 
 const modelFiles = globSync(`${__dirname}/**/*.model.{ts,js}`);
 
-const modelsList: String[] = modelFiles.map(file => {
-    const modelFileNameWithoutExtension: String = basename(file).split('.')[0];
+const modelsList: string[] = modelFiles.map(file => {
+    const modelFileNameWithoutExtension: string = basename(file).split('.')[0];
     return modelFileNameWithoutExtension.split('-').map((modelNamePart) => {
         return modelNamePart[0].toUpperCase() + modelNamePart.slice(1);
     }).join('');
