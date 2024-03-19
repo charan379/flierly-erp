8fe30a7a8561c650cb6ebb5bf6c64296484c8bea
charan379 <37008444+charan379@users.noreply.github.com>
2024-03-19 16:49:05 +0530

    minor tweaks

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import createHttpError from "http-errors";
 import router from "@/routes";
 import HttpCodes from "@/constants/httpCodes";
 import errorHandler from "@/middlewares/error-handler.middleware";
+import { modelsList } from "./models";
 
 // create express application instance
 const app: Express = express();
