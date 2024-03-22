cd75a23dbc866312e3012019da4d96c47bf485a1
charan379 <37008444+charan379@users.noreply.github.com>
2024-03-22 16:42:09 +0530

    minor tweaks

--- backend/src/lib/crud-controller/index.ts
+++ backend/src/lib/crud-controller/index.ts
@@ -4,7 +4,6 @@ import create from "./create";
 import { Request, Response } from "express";
 import read from "./read";
 
-
 const CRUDController = async (modelName: string) => {
 
     const modelDetails = await getModelsList().then((models) =>
