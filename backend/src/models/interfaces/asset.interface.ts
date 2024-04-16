d2681081cd9e6960f184211f921f74d788cd3054
charan379 <37008444+charan379@users.noreply.github.com>
2024-04-16 14:45:25 +0530

    added asset model

---
+++ backend/src/models/interfaces/asset.interface.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export interface Asset {
+    _id: mongoose.ObjectId,
+    isDeleted: boolean,
+    isActive: boolean,
+    product: mongoose.ObjectId,
+    imei1?: string,
+    imei2?: string,
+    serialNumber: string,
+    manufacturingDate: Date;
+};
