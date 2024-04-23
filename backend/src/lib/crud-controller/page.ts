f503b2e84d4f3819e6a41eec4561f8c3a65935ba
charan379 <37008444+charan379@users.noreply.github.com>
2024-04-23 16:06:52 +0530

    added query support on page request

--- backend/src/lib/crud-controller/page.ts
+++ backend/src/lib/crud-controller/page.ts
@@ -1,27 +1,55 @@
 import HttpCodes from "@/constants/httpCodes";
+import JoiSchemaValidator from "@/utils/joi-schema.validator";
+import buildMongoQuery from "@/utils/mongo-query.builder";
+import buildMongoSortObject, { SortObject } from "@/utils/mongo-sort.builder";
 import pageResponseBuilder from "@/utils/page-response.builder";
 import { NextFunction, Request, Response } from "express";
+import Joi from "joi";
 import mongoose from "mongoose";
 
+export interface PageRequest {
+    fields: string,
+    queries: string,
+    sort: string,
+    page: number,
+    limit: number
+}
+
+const pageRequestSchema: Joi.ObjectSchema<PageRequest> = Joi.object({
+    fields: Joi.string().default('name'),
+    queries: Joi.string().default(' '),
+    sort: Joi.string().default('createdAt.asc'),
+    page: Joi.number().integer().min(1).default(1),
+    limit: Joi.number().integer().min(1).default(20),
+});
 
 const page = async (model: mongoose.Model<any>, req: Request, res: Response, next: NextFunction) => {
     try {
-        const page: number = parseInt(req.query.page as string) || 1;
-        const limit: number = parseInt(req.query.limit as string) || 10;
+
+        const pageRequest: PageRequest = await JoiSchemaValidator(pageRequestSchema, req.query, { allowUnknown: false, abortEarly: false }, "Dynamic page request API.");
+
+        const query: { $and: object[] } = buildMongoQuery(pageRequest.fields.split(","), pageRequest.queries.split(","));
+
+        const sort: SortObject = buildMongoSortObject(pageRequest.sort);
+
+        const page: number = pageRequest.page;
+        const limit: number = pageRequest.limit;
         const skip: number = (page * limit) - limit;
 
-        const resultsPromise = model.find({ isDeleted: false }, { __v: 0 })
+        const resultsPromise = model.find({ ...query }, { __v: 0 })
+            .where("isDeleted", false)
+            .sort({ ...sort })
             .skip(skip)
             .limit(limit)
             .exec();
 
-        const countPromise = model.countDocuments(
-            { isDeleted: false }
-        ).exec();
+        const countPromise = model.countDocuments({ ...query })
+            .where("isDeleted", false)
+            .exec();
 
         const [results, count] = await Promise.all([resultsPromise, countPromise]);
 
-        const re: PageResult = await pageResponseBuilder(results, page, limit, count, {});
+        const re: PageResult = await pageResponseBuilder(results, page, limit, count, sort);
 
         return res.status(HttpCodes.OK).json(re);
 
