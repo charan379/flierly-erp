aa760600b61b89577a67756d3aa39c9c47e43b73
charan379 <37008444+charan379@users.noreply.github.com>
2024-05-21 15:41:39 +0530

    added prompts

--- backend/src/setup/generate-super-admin.ts
+++ backend/src/setup/generate-super-admin.ts
@@ -1,16 +1,39 @@
 import AccessGroupModel from "@/models/access-group.model";
 import { AccessGroup } from "@/models/interfaces/access-group.interface";
 import PermissionModel from "@/models/permission.model";
+import { string } from "joi";
 import mongoose from "mongoose";
+import prompter from "prompts";
 
 
 
 async function generateSuperAdmin(): Promise<void> {
-    let superAdminGroup: AccessGroup | null = await AccessGroupModel.findOne({ name: 'super-admin' }).exec();
+    let superAdminGroup: AccessGroup | null = await AccessGroupModel.findOne({ code: 'super-admin' }).exec();
 
     if (superAdminGroup === null) {
         superAdminGroup = await generateSuperAdminGroup();
     };
+
+    const response: { username: string, password: string, confirm_password: string } = await prompter([{
+        type: 'text',
+        name: 'username',
+        message: 'Username for super admin: ',
+        validate: value => /^([a-z0-9&.-]){5,25}$/.test(value) ? true : 'Invalid username',
+    },
+    {
+        type: 'password',
+        name: 'password',
+        message: 'Password for super admin: ',
+        validate: value => /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,28}$/.test(value) ? true : 'Invalid password'
+    },
+    {
+        type: 'password',
+        name: 'confirm_password',
+        message: 'Confirm password for super admin: ',
+        validate: value => value === response.password ? true : "Password doesn't match"
+    }]);
+
+    console.log(response)
 
 }
 
