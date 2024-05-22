6ed63d1e486222a51c196a54338cdd30040c2c64
charan379 <37008444+charan379@users.noreply.github.com>
2024-05-22 16:00:26 +0530

    minor tweaks

--- backend/src/setup/generate-super-admin.ts
+++ backend/src/setup/generate-super-admin.ts
@@ -1,11 +1,8 @@
+import userDetailsPrompt from "@/setup/prompts/user-details.prompt";
 import AccessGroupModel from "@/models/access-group.model";
 import { AccessGroup } from "@/models/interfaces/access-group.interface";
 import PermissionModel from "@/models/permission.model";
-import { string } from "joi";
 import mongoose from "mongoose";
-import prompter from "prompts";
-
-
 
 async function generateSuperAdmin(): Promise<void> {
     let superAdminGroup: AccessGroup | null = await AccessGroupModel.findOne({ code: 'super-admin' }).exec();
@@ -14,27 +11,9 @@ async function generateSuperAdmin(): Promise<void> {
         superAdminGroup = await generateSuperAdminGroup();
     };
 
-    const response: { username: string, password: string, confirm_password: string } = await prompter([{
-        type: 'text',
-        name: 'username',
-        message: 'Username for super admin: ',
-        validate: value => /^([a-z0-9&.-]){5,25}$/.test(value) ? true : 'Invalid username',
-    },
-    {
-        type: 'password',
-        name: 'password',
-        message: 'Password for super admin: ',
-        validate: value => /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,28}$/.test(value) ? true : 'Invalid password'
-    },
-    {
-        type: 'password',
-        name: 'confirm_password',
-        message: 'Confirm password for super admin: ',
-        validate: value => value === response.password ? true : "Password doesn't match"
-    }]);
+    const credsPrompt = await userDetailsPrompt();
 
-    console.log(response)
-
+    console.log(credsPrompt);
 }
 
 async function generateSuperAdminGroup(): Promise<AccessGroup> {
@@ -47,5 +26,6 @@ async function generateSuperAdminGroup(): Promise<AccessGroup> {
     });
     return superAdminGroup;
 }
+
 
 export default generateSuperAdmin;
