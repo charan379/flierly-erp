8b089c4ba0ff6aecd0d41e758c8b3bca2098f63d
Charanteja <37008444+charan379@users.noreply.github.com>
2024-05-22 14:01:53 +0000

    added permission related fields to user interface.

--- backend/src/models/interfaces/user.interface.ts
+++ backend/src/models/interfaces/user.interface.ts
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import { AccessGroup } from "./access-group.interface";
+import { Permission } from "./permission.interface";
 
 export interface User {
     _id: mongoose.ObjectId,
@@ -8,6 +10,9 @@ export interface User {
     password: string,
     email: string,
     mobile: string,
+    permissions: mongoose.ObjectId[] | Permission[],
+    excludedPermissions: mongoose.ObjectId[] | Permission[],
+    accecesGroups: mongoose.ObjectId[] | AccessGroup[],
     createdAt: Date,
     updatedAt: Date,
 }
