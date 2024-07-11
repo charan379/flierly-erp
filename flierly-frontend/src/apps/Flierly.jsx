a88c59318aacf58cd458b703abc8645e3d5e782a
charan379 <37008444+charan379@users.noreply.github.com>
2024-07-11 11:24:47 +0530

    wrap erpApp in Antd locale

--- flierly-frontend/src/apps/Flierly.jsx
+++ flierly-frontend/src/apps/Flierly.jsx
@@ -1,15 +1,17 @@
 import React, { Suspense } from 'react'
 import ErpApp from './ErpApp';
 import PageLoader from '@/components/PageLoader';
+import AntdConfigProvider from '@/locale/AntdConfigProvider';
 
 const DefaultApp = () => {
   return (
-
-    <Suspense fallback={<PageLoader />}>
-      <ErpApp />
-    </Suspense>
+    <AntdConfigProvider>
+      <Suspense fallback={<PageLoader />}>
+        <ErpApp />
+      </Suspense>
+    </AntdConfigProvider>
   )
-}
+};
 
 function Flierly() {
 
@@ -18,6 +20,6 @@ function Flierly() {
   return (
     <div>FlierlyApp</div>
   )
-}
+};
 
 export default Flierly
