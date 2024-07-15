c96d8145a8d66ef12acafe16b6ab967a1daa6c50
charan379 <37008444+charan379@users.noreply.github.com>
2024-07-15 11:55:39 +0530

    LoginForm updated

--- flierly-frontend/src/forms/LoginForm.jsx
+++ flierly-frontend/src/forms/LoginForm.jsx
@@ -1,11 +1,13 @@
+import React from "react";
 import useLocale from "@/redux/locale/useLocale";
 import {
   InfoCircleOutlined,
   LockOutlined,
   MailOutlined,
 } from "@ant-design/icons";
-import { Checkbox, Form, Input } from "antd";
-import React from "react";
+import { Button, Checkbox, Form, Input } from "antd";
+import Loading from "@/components/Loading";
+import { useNavigate } from "react-router-dom";
 
 /**
  * LoginForm component to display a login form with email and password fields.
@@ -14,58 +16,88 @@ import React from "react";
  */
 const LoginForm = () => {
   const { translate, langDirection } = useLocale(); // Using the custom hook to get translation function and language direction
-
+  const navigate = useNavigate();
   return (
-    <div style={{ direction: langDirection }}> {/* Setting text direction based on language */}
-      <Form.Item
-        label={translate("email")} // Translated label for email field
-        name="email" // Name of the form field
-        rules={[{ required: true }, { type: "email" }]} // Validation rules
-        tooltip={{
-          title: translate("email_is_required"), // Tooltip text
-          icon: <InfoCircleOutlined />, // Tooltip icon
+    <Loading isLoading={false}>
+      <Form
+        layout="vertical"
+        name="login_form"
+        className="login-form"
+        initialValues={{
+          remember: true,
         }}
+        onFinish={() => console.log("onFinish completed")}
       >
-        <Input
-          prefix={<MailOutlined className="site-form-item-icon" />} // Prefix icon for the input field
-          placeholder={translate("email")} // Translated placeholder text
-          type="email" // Input type
-          size="large" // Size of the input field
-        />
-      </Form.Item>
+        <div style={{ direction: langDirection }}> {/* Setting text direction based on language */}
+          {/* Email */}
+          <Form.Item
+            label={translate("email")} // Translated label for email field
+            name="email" // Name of the form field
+            rules={[ // Validation rules
+              { required: true, message: translate("email_is_required") },
+              { type: "email" }
+            ]}
+            tooltip={{
+              title: translate("email_is_required"), // Tooltip text
+              icon: <InfoCircleOutlined />, // Tooltip icon
+            }}
+          >
+            <Input
+              prefix={<MailOutlined className="site-form-item-icon" />} // Prefix icon for the input field
+              placeholder={translate("email")} // Translated placeholder text
+              type="email" // Input type
+              size="large" // Size of the input field
+            />
+          </Form.Item>
+          {/* password */}
+          <Form.Item
+            label={translate("password")} // Translated label for password field
+            name="password" // Name of the form field
+            rules={[ // Validation rules
+              { required: true, message: translate('password_is_required') }
+            ]}
+            tooltip={{
+              title: translate('password_is_required'), // Tooltip text
+              icon: <InfoCircleOutlined />, // Tooltip icon
+            }}
+          >
+            <Input.Password
+              prefix={<LockOutlined className="site-form-item-icon" />} // Prefix icon for the password field
+              placeholder={translate("password")} // Translated placeholder text
+              size="large" // Size of the input field
+            />
+          </Form.Item>
 
-      <Form.Item
-        label={translate("password")} // Translated label for password field
-        name="password" // Name of the form field
-        rules={[{ required: true }]} // Validation rules
-        tooltip={{
-          title: translate('password_is_required'), // Tooltip text
-          icon: <InfoCircleOutlined />, // Tooltip icon
-        }}
-      >
-        <Input.Password
-          prefix={<LockOutlined className="site-form-item-icon" />} // Prefix icon for the password field
-          placeholder={translate("password")} // Translated placeholder text
-          size="large" // Size of the input field
-        />
-      </Form.Item>
-
-      <Form.Item>
-        <Form.Item name="remember" valuePropName="checked" noStyle  /* Remember me checkbox */>
-          <Checkbox>{translate("remember_me")}</Checkbox>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle  /* Remember me checkbox */>
+              <Checkbox>{translate("remember_me")}</Checkbox>
+            </Form.Item>
+            {/*  */}
+            <a
+              className="login-form-forgot" // CSS class for the forgot password link
+              href="/forgetpassword" // Link to forgot password page
+              style={{ marginLeft: langDirection === "rtl" ? "220px" : "0px" }} // Conditional margin based on language direction
+            >
+              {translate("forgot_password")} {/* Translated link text */}
+            </a>
+            {/*  */}
+          </Form.Item>
+        </div>
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+            loading={false}
+            size="large"
+          >
+            {translate("sign_in")}
+          </Button>
+          {translate('or')} <a href="/register"> {translate('register_now')} </a>
         </Form.Item>
-        {/*  */}
-        <a
-          className="login-form-forgot" // CSS class for the forgot password link
-          href="/forgetpassword" // Link to forgot password page
-          style={{ marginLeft: langDirection === "rtl" ? "220px" : "0px" }} // Conditional margin based on language direction
-        >
-          {translate("forgot_password")} {/* Translated link text */}
-        </a>
-        {/*  */}
-      </Form.Item>
-    </div>
+      </Form>
+    </Loading>
   );
 };
 
-export default LoginForm; // Exporting the component as default
+export default LoginForm;
--- flierly-frontend/src/pages/Login/Login.jsx
+++ flierly-frontend/src/pages/Login/Login.jsx
@@ -1,47 +1,9 @@
-import Loading from "@/components/Loading";
-import LoginForm from "@/forms/LoginForm";
 import AuthModule from "@/modules/Auth/AuthModule";
-import useLocale from "@/redux/locale/useLocale";
-import { Button, Form } from "antd";
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import LoginForm from "@/forms/LoginForm";
 
 const Login = () => {
-  const navigate = useNavigate();
-
-  return <AuthModule authForm={<FormContainer />} AUTH_TITLE="sign_in" />;
-};
-
-const FormContainer = () => {
-  const { translate, langDirection } = useLocale();
-
-  return (
-    <Loading isLoading={false}>
-      <Form
-        layout="vertical"
-        name="login_form"
-        className="login-form"
-        initialValues={{
-          remember: true,
-        }}
-        onFinish={() => console.log("onFinish completed")}
-      >
-        <LoginForm />
-        <Form.Item>
-          <Button
-            type="primary"
-            htmlType="submit"
-            className="login-form-button"
-            loading={false}
-            size="large"
-          >
-            {translate("sign_in")}
-          </Button>
-          {translate('or')} <a href="/register"> {translate('register_now')} </a>
-        </Form.Item>
-      </Form>
-    </Loading>
-  );
+  return <AuthModule authForm={<LoginForm />} AUTH_TITLE="sign_in" />;
 };
 
 export default Login;
