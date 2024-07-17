8886c784caf04459215953ded0750b0c63f54ba3
charan379 <37008444+charan379@users.noreply.github.com>
2024-07-17 16:15:32 +0530

    added feature : `LangSelector`

---
+++ flierly-frontend/src/features/Language/LangSelector.jsx
@@ -0,0 +1,36 @@
+import useResponsive from '@/hooks/useResponsive';
+import { Select } from 'antd';
+import React from 'react';
+import languageOptions from './languageOptions';
+import useLocale from '@/redux/locale/useLocale';
+
+const LangSelector = () => {
+
+    const { isMobile } = useResponsive();
+    const { langCode, changeLanguage } = useLocale();
+    return (
+        <Select
+            showSearch
+            defaultValue={langCode}
+            loading={false}
+            options={languageOptions()}
+            onChange={(newLangCode) => changeLanguage(newLangCode)}
+            filterOption={(input, option) =>
+                (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
+            }
+            filterSort={(optionA, optionB) =>
+                (optionA?.label ?? '').toLowerCase().startsWith((optionB?.label ?? '').toLowerCase())
+            }
+            style={{
+                width: isMobile ? '90px' : '130px',
+                float: 'right',
+                marginTop: '5px',
+                cursor: 'pointer',
+                direction: 'ltr',
+            }}
+            size='large'
+        ></Select>
+    )
+}
+
+export default LangSelector
