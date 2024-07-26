fedae48cb18868ee3c9052bb82c7f05edd018988
charan379 <37008444+charan379@users.noreply.github.com>
2024-07-26 16:27:26 +0530

    added auth reducer

--- flierly-frontend/src/redux/store.js
+++ flierly-frontend/src/redux/store.js
@@ -2,6 +2,7 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { localStorageHealthCheck } from "./statePersist";
 import localeReducer from "./locale/reducer";
 import themeReducer from "./theme/reducer";
+import authReducer from "@/modules/auth/redux/auth/reducer";
 
 // Performing a health check for localStorage state persistence
 localStorageHealthCheck();
@@ -12,6 +13,7 @@ localStorageHealthCheck();
 const rootReducer = combineReducers({
   locale: localeReducer,
   theme: themeReducer,
+  auth: authReducer,
 });
 
 /**
