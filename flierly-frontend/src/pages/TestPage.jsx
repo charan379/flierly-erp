489c11f93c099b9f264383d6d12beaec458abc2b
charan379 <37008444+charan379@users.noreply.github.com>
2024-07-30 15:29:27 +0530

    test page

---
+++ flierly-frontend/src/pages/TestPage.jsx
@@ -0,0 +1,45 @@
+import useLocale from "@/locale/useLocale";
+import { useState } from "react";
+import reactLogo from "@/assets/react.svg";
+
+export default function TestPage() {
+  const [count, setCount] = useState(0);
+
+  const { locale, translate, resetLocale, setLanguage } = useLocale();
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <button onClick={() => resetLocale()}>Reset Language</button>
+        <button onClick={() => setLanguage("te_in")}>Change Language</button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+        <ul>
+          {Object.entries(locale.translation).map((entry) => {
+            return (
+              <li key={entry[0]}>
+                <b>{entry[0]}</b> : {translate(entry[0])}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
