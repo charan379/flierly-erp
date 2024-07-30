4e4eeb70231bab91cc2e16f58973415e87b1cec7
charan379 <37008444+charan379@users.noreply.github.com>
2024-07-30 16:59:48 +0530

    app layout updated

--- flierly-frontend/src/layout/AppLayout.jsx
+++ flierly-frontend/src/layout/AppLayout.jsx
@@ -14,14 +14,14 @@ export default function AppLayout() {
         margin: "40px auto 30px",
         overflow: "initial",
         width: "100%",
-        padding: "0 25px",
+        padding: "0 50px",
         maxWidth: "none",
       }
     : {
         margin: "40px auto 30px",
         overflow: "initial",
         width: "100%",
-        padding: "0 50px",
+        padding: "0 25px",
         maxWidth: 1400,
       };
 
@@ -38,7 +38,22 @@ export default function AppLayout() {
         }}
       >
         <Sidebar />
-        <Layout.Content style={contentStyle}>
+        <Layout.Content
+          style={{
+            ...contentStyle,
+            // background: "var(--bg-color-primary-flierly)",
+            // boxShadow: "var(--floating-section-box-shadow)",
+            height: "84dvh",
+            margin: "5px 10px 5px 5px",
+            borderRadius: "10px",
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+            flexWrap: "wrap",
+            overflow: "auto",
+          }}
+        >
           <Outlet />
         </Layout.Content>
       </Layout>
