49229df4e29057d7a070bb24354ab9d019997909
charan379 <37008444+charan379@users.noreply.github.com>
2024-07-30 17:01:04 +0530

    test page updated

--- flierly-frontend/src/pages/TestPage.jsx
+++ flierly-frontend/src/pages/TestPage.jsx
@@ -9,37 +9,366 @@ export default function TestPage() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <button onClick={() => resetLocale()}>Reset Language</button>
-        <button onClick={() => setLanguage("te_in")}>Change Language</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        <ul>
-          {Object.entries(locale.translation).map((entry) => {
-            return (
-              <li key={entry[0]}>
-                <b>{entry[0]}</b> : {translate(entry[0])}
-              </li>
-            );
-          })}
-        </ul>
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <button onClick={() => resetLocale()}>Reset Language</button>
+          <button onClick={() => setLanguage("te_in")}>Change Language</button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+          <ul>
+            {Object.entries(locale.translation).map((entry) => {
+              return (
+                <li key={entry[0]}>
+                  <b>{entry[0]}</b> : {translate(entry[0])}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="card">
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <button onClick={() => resetLocale()}>Reset Language</button>
+          <button onClick={() => setLanguage("te_in")}>Change Language</button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+          <ul>
+            {Object.entries(locale.translation).map((entry) => {
+              return (
+                <li key={entry[0]}>
+                  <b>{entry[0]}</b> : {translate(entry[0])}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+      <div className="card">
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <button onClick={() => resetLocale()}>Reset Language</button>
+          <button onClick={() => setLanguage("te_in")}>Change Language</button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+          <ul>
+            {Object.entries(locale.translation).map((entry) => {
+              return (
+                <li key={entry[0]}>
+                  <b>{entry[0]}</b> : {translate(entry[0])}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+      <div className="card">
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <button onClick={() => resetLocale()}>Reset Language</button>
+          <button onClick={() => setLanguage("te_in")}>Change Language</button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+          <ul>
+            {Object.entries(locale.translation).map((entry) => {
+              return (
+                <li key={entry[0]}>
+                  <b>{entry[0]}</b> : {translate(entry[0])}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+      <div className="card">
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <button onClick={() => resetLocale()}>Reset Language</button>
+          <button onClick={() => setLanguage("te_in")}>Change Language</button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+          <ul>
+            {Object.entries(locale.translation).map((entry) => {
+              return (
+                <li key={entry[0]}>
+                  <b>{entry[0]}</b> : {translate(entry[0])}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+      <div className="card">
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <button onClick={() => resetLocale()}>Reset Language</button>
+          <button onClick={() => setLanguage("te_in")}>Change Language</button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+          <ul>
+            {Object.entries(locale.translation).map((entry) => {
+              return (
+                <li key={entry[0]}>
+                  <b>{entry[0]}</b> : {translate(entry[0])}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+      <div className="card">
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <button onClick={() => resetLocale()}>Reset Language</button>
+          <button onClick={() => setLanguage("te_in")}>Change Language</button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+          <ul>
+            {Object.entries(locale.translation).map((entry) => {
+              return (
+                <li key={entry[0]}>
+                  <b>{entry[0]}</b> : {translate(entry[0])}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+      <div className="card">
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <button onClick={() => resetLocale()}>Reset Language</button>
+          <button onClick={() => setLanguage("te_in")}>Change Language</button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+          <ul>
+            {Object.entries(locale.translation).map((entry) => {
+              return (
+                <li key={entry[0]}>
+                  <b>{entry[0]}</b> : {translate(entry[0])}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+      <div className="card">
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <button onClick={() => resetLocale()}>Reset Language</button>
+          <button onClick={() => setLanguage("te_in")}>Change Language</button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+          <ul>
+            {Object.entries(locale.translation).map((entry) => {
+              return (
+                <li key={entry[0]}>
+                  <b>{entry[0]}</b> : {translate(entry[0])}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+      <div className="card">
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <button onClick={() => resetLocale()}>Reset Language</button>
+          <button onClick={() => setLanguage("te_in")}>Change Language</button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+          <ul>
+            {Object.entries(locale.translation).map((entry) => {
+              return (
+                <li key={entry[0]}>
+                  <b>{entry[0]}</b> : {translate(entry[0])}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+      <div className="card">
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <button onClick={() => resetLocale()}>Reset Language</button>
+          <button onClick={() => setLanguage("te_in")}>Change Language</button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+          <ul>
+            {Object.entries(locale.translation).map((entry) => {
+              return (
+                <li key={entry[0]}>
+                  <b>{entry[0]}</b> : {translate(entry[0])}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+      <div className="card">
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <button onClick={() => resetLocale()}>Reset Language</button>
+          <button onClick={() => setLanguage("te_in")}>Change Language</button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+          <ul>
+            {Object.entries(locale.translation).map((entry) => {
+              return (
+                <li key={entry[0]}>
+                  <b>{entry[0]}</b> : {translate(entry[0])}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
