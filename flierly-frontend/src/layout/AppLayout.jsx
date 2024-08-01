f195021ab0006054df68f726f43f7c17516e18b7
charan379 <37008444+charan379@users.noreply.github.com>
2024-08-01 18:06:51 +0530

    app layout updated

--- flierly-frontend/src/layout/AppLayout.jsx
+++ flierly-frontend/src/layout/AppLayout.jsx
@@ -1,4 +1,3 @@
-import useResponsive from "@/hooks/useResponsive";
 import useLocale from "@/locale/useLocale";
 import { Layout } from "antd";
 import Header from "./Header";
@@ -7,25 +6,6 @@ import Sidebar from "./Sidebar";
 
 export default function AppLayout() {
   const { langDirection } = useLocale();
-  const { isMobile } = useResponsive();
-
-  const contentStyle = isMobile
-    ? {
-        margin: "40px auto 30px",
-        overflow: "initial",
-        width: "100%",
-        padding: "0 50px",
-        maxWidth: "none",
-      }
-    : {
-        margin: "40px auto 30px",
-        overflow: "initial",
-        width: "100%",
-        padding: "0 25px",
-        maxWidth: 1400,
-      };
-
-  const layoutStyle = isMobile ? { marginLeft: 0 } : {};
 
   return (
     <Layout>
@@ -33,25 +13,24 @@ export default function AppLayout() {
       <Layout
         hasSider
         style={{
-          ...layoutStyle,
           flexDirection: langDirection === "rtl" ? "row-reverse" : "row",
         }}
       >
         <Sidebar />
         <Layout.Content
           style={{
-            ...contentStyle,
+            width: "100%",
             // background: "var(--bg-color-primary-flierly)",
             // boxShadow: "var(--floating-section-box-shadow)",
             height: "84dvh",
             margin: "5px 10px 5px 5px",
-            borderRadius: "10px",
+            // borderRadius: "10px",
             display: "flex",
-            flexDirection: "row",
+            // flexDirection: "row",
             alignItems: "center",
             justifyContent: "center",
             flexWrap: "wrap",
-            overflow: "auto",
+            overflow: "visible",
           }}
         >
           <Outlet />
