a82e28e6aeab9fcf40f10a5acebdd921f430c044
charan379 <37008444+charan379@users.noreply.github.com>
2024-08-20 19:13:10 +0530

    redirect after login

--- flierly-frontend/src/apps/Flierly.jsx
+++ flierly-frontend/src/apps/Flierly.jsx
@@ -1,25 +1,26 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import AntdConfigProvider from "@/theme/AntdConfigProvider";
 import AuthRouter from "@/modules/auth/router/AuthRouter";
 import { useAuth } from "@/modules/auth/hooks/useAuth";
 import AppRouter from "@/router/Router";
+import { useNavigate } from "react-router-dom";
+import { loadingTypes } from "@/types/loading";
 
 function Flierly() {
-  const { isLoggedIn } = useAuth();
+  const { isLoggedIn, loading } = useAuth();
+  const navigate = useNavigate();
 
-  if (isLoggedIn) {
-    return (
-      <AntdConfigProvider>
-        <AppRouter />
-      </AntdConfigProvider>
-    );
-  } else {
-    return (
-      <AntdConfigProvider>
-        <AuthRouter />
-      </AntdConfigProvider>
-    );
-  }
+  useLayoutEffect(() => {
+    if (loading === loadingTypes.SUCCEEDED) {
+      navigate("/");
+    }
+  }, [loading]);
+
+  return (
+    <AntdConfigProvider>
+      {isLoggedIn ? <AppRouter /> : <AuthRouter />}
+    </AntdConfigProvider>
+  );
 }
 
 export default Flierly;
