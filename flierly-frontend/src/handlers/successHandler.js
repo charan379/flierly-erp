7bf1b63d66d73825aa98ce808ec1ad70d356260e
charan379 <37008444+charan379@users.noreply.github.com>
2024-08-22 13:14:07 +0530

    optimize success handler

--- flierly-frontend/src/handlers/successHandler.js
+++ flierly-frontend/src/handlers/successHandler.js
@@ -1,28 +1,17 @@
 import { notification } from "antd";
 import codeMessage from "./codeMessage";
+import { message as antdMessage } from "antd";
 
 /**
- * Handles the success and error notifications based on the response.
+ * Handles the success notification.
  *
- * @param {Object} [response={ data: any, status: number }] - The response object from the server.
- * @param {any} response.data
- * @param {number} response.status
- * @param {Object} [options={ notifyOnSuccess: false, notifyOnFailed: true }] - Notification options.
- * @param {boolean} options.notifyOnSuccess - Flag to notify on success.
- * @param {boolean} options.notifyOnFailed - Flag to notify on failure.
+ * @param {string} notifyType - The type of notification to use ("toast" or "message").
+ * @param {string} httpCode - The HTTP status code to display in the notification.
+ * @param {string} message - The message to display in the notification.
  */
-const successHandler = (response, options = { notifyOnSuccess: false, notifyOnFailed: true }) => {
-  const { data, status } = response; // Destructure data and status from response
-  const { notifyOnSuccess, notifyOnFailed } = options; // Destructure notification options
-
-  // Extract message and httpCode from response data
-  const message = data?.message || codeMessage[status];
-  const httpCode = data?.httpCode || status;
-
-  // Check if the response indicates a successful operation
-  if (data?.success) {
-    if (notifyOnSuccess) {
-      // If notifications on success are enabled
+const handleSuccessNotification = (notifyType, httpCode, message) => {
+  switch (notifyType) {
+    case "toast":
       notification.config({
         duration: 2, // Duration the notification will be displayed
         maxCount: 3, // Maximum number of notifications displayed at once
@@ -31,13 +20,25 @@ const successHandler = (response, options = { notifyOnSuccess: false, notifyOnFa
         message: `${httpCode} : Request success`, // Title of the success notification with httpCode
         description: message, // Description of the success notification
       });
-    }
-  } else {
-    // If the response indicates a failed operation and notifications on failure are enabled
-    if (notifyOnFailed) {
-      // Use the error message if available, otherwise use the default code message
-      const errorMessage = data?.error?.message || message;
+      break;
+    case "message":
+      antdMessage.success(message);
+      break;
+    default:
+      break;
+  }
+};
 
+/**
+ * Handles the error notification.
+ *
+ * @param {string} notifyType - The type of notification to use ("toast" or "message").
+ * @param {string} httpCode - The HTTP status code to display in the notification.
+ * @param {string} errorMessage - The error message to display in the notification.
+ */
+const handleErrorNotification = (notifyType, httpCode, errorMessage) => {
+  switch (notifyType) {
+    case "toast":
       notification.config({
         duration: 4, // Duration the notification will be displayed
         maxCount: 2, // Maximum number of notifications displayed at once
@@ -46,6 +47,51 @@ const successHandler = (response, options = { notifyOnSuccess: false, notifyOnFa
         message: `${httpCode} : Request failed`, // Title of the error notification with status code
         description: errorMessage, // Description of the error notification
       });
+      break;
+    case "message":
+      antdMessage.error(errorMessage);
+      break;
+    default:
+      break;
+  }
+};
+
+/**
+ * Handles the success and error notifications based on the server response.
+ *
+ * @param {Object} [response={ data: any, status: number }] - The response object from the server.
+ * @param {any} response.data - The data returned from the server.
+ * @param {number} response.status - The HTTP status code of the response.
+ * @param {Object} [options={ notifyOnSuccess: false, notifyOnFailed: true, notifyType: "toast" }] - Options for customizing the notification behavior.
+ * @param {boolean} options.notifyOnSuccess - Flag to indicate whether to notify on success.
+ * @param {boolean} options.notifyOnFailed - Flag to indicate whether to notify on failure.
+ * @param {string} options.notifyType - Type of notification to use: "toast" for Ant Design's notification or "message" for message notifications.
+ */
+const successHandler = (
+  response,
+  options = {
+    notifyOnSuccess: false,
+    notifyOnFailed: true,
+    notifyType: "toast",
+  }
+) => {
+  // Destructure data and status from response
+  const { data, status } = response;
+
+  // Destructure notification options
+  const { notifyOnSuccess = false, notifyOnFailed = true, notifyType = "toast" } = options;
+
+  // Extract message and HTTP status code from response data, fallback to default codeMessage if not available
+  const message = data?.message || codeMessage[status];
+  const httpCode = data?.httpCode || status;
+
+  // Check if the response indicates a successful operation
+  if (data?.success) {
+    if (notifyOnSuccess) handleSuccessNotification(notifyType, httpCode, message); // Trigger success notification if enabled
+  } else {
+    if (notifyOnFailed) {
+      const errorMessage = data?.error?.message || message; // Use the error message if available, otherwise use the default message
+      handleErrorNotification(notifyType, httpCode, errorMessage); // Trigger error notification if enabled
     }
   }
 };
