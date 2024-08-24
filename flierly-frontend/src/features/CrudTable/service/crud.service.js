bcf339cfc300b40832b06439399ebdf46ca1a2f8
charan379 <37008444+charan379@users.noreply.github.com>
2024-08-24 17:07:48 +0530

    optimzed crud service

--- flierly-frontend/src/features/CrudTable/service/crud.service.js
+++ flierly-frontend/src/features/CrudTable/service/crud.service.js
@@ -7,127 +7,62 @@ import axios from "axios";
 const api = axios.create({
   baseURL: serverConfig.BASE_API_URL,
   headers: {
-    Authorization: "Bearer " + statePersist.get("auth")?.token,
+    Authorization: `Bearer ${statePersist.get("auth")?.token}`,
   },
 });
 
+// Function to handle API responses
+const handleResponse = async (promise, notifyOnSuccess, notifyOnFailed) => {
+  try {
+    const { data, status } = await promise;
+    successHandler(
+      { data, status },
+      {
+        notifyOnSuccess,
+        notifyOnFailed,
+        notifyType: "message",
+      }
+    );
+    return data;
+  } catch (error) {
+    return errorHandler(error);
+  }
+};
+
 const crudService = {
-  // page
+  // Fetch paginated documents
   page: async ({
     entity,
     autopopulate = false,
     pagination = { limit: 10, page: 1 },
   }) => {
-    try {
-      // Sending GET request to fetch documents page
-      const response = await api.get(`/${entity}/page`, {
-        params: { autopopulate: autopopulate, ...pagination },
-      });
-
-      // Destructuring data and status from response
-      const { data, status } = response;
-
-      // Handling success response
-      successHandler(
-        { data, status },
-        {
-          notifyOnSuccess: false, // Disable success notification
-          notifyOnFailed: true, // Enable failure notification
-        }
-      );
-
-      // Returning response data
-      return data;
-    } catch (error) {
-      // Handling error response
-      return errorHandler(error);
-    }
+    const promise = api.get(`/${entity}/page`, {
+      params: { autopopulate, ...pagination },
+    });
+    return handleResponse(promise, false, true);
   },
 
-  // delete
+  // Soft delete documents
   delete: async ({ entity, docIds = [] }) => {
-    try {
-      // Sending DELETE request to soft delete documents
-      const response = await api.delete(`/${entity}/delete`, {
-        data: [...docIds],
-      });
-
-      // Destructuring data and status from response
-      const { data, status } = response;
-
-      // Handling success response
-      successHandler(
-        { data, status },
-        {
-          notifyOnSuccess: true, // Disable success notification
-          notifyOnFailed: true, // Enable failure notification
-          notifyType: "message",
-        }
-      );
-
-      // Returning response data
-      return data;
-    } catch (error) {
-      // Handling error response
-      return errorHandler(error);
-    }
+    const promise = api.delete(`/${entity}/delete`, {
+      data: docIds,
+    });
+    return handleResponse(promise, true, true);
   },
 
-  // activate
+  // Activate or inactivate documents
   activate: async ({ entity, docIds = [], action = "activate" }) => {
-    try {
-      // Sending PUT request to activate / inactivate documents
-      const response = await api.put(`/${entity}/activate`, {
-        ids: docIds,
-        action,
-      });
-
-      // Destructuring data and status from response
-      const { data, status } = response;
-
-      // Handling success response
-      successHandler(
-        { data, status },
-        {
-          notifyOnSuccess: true, // Disable success notification
-          notifyOnFailed: true, // Enable failure notification
-          notifyType: "message",
-        }
-      );
-
-      // Returning response data
-      return data;
-    } catch (error) {
-      // Handling error response
-      return errorHandler(error);
-    }
+    const promise = api.put(`/${entity}/activate`, {
+      ids: docIds,
+      action,
+    });
+    return handleResponse(promise, true, true);
   },
 
-  // restore
+  // Restore documents
   restore: async ({ entity, docIds = [] }) => {
-    try {
-      // Sending PUT request to restore documents
-      const response = await api.put(`/${entity}/restore`, [...docIds], {});
-
-      // Destructuring data and status from response
-      const { data, status } = response;
-
-      // Handling success response
-      successHandler(
-        { data, status },
-        {
-          notifyOnSuccess: true, // Disable success notification
-          notifyOnFailed: true, // Enable failure notification
-          notifyType: "message",
-        }
-      );
-
-      // Returning response data
-      return data;
-    } catch (error) {
-      // Handling error response
-      return errorHandler(error);
-    }
+    const promise = api.put(`/${entity}/restore`, docIds);
+    return handleResponse(promise, true, true);
   },
 };
 
