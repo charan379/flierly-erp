0753222a9840c504f1012be23803d877a3c4b5d3
charan379 <37008444+charan379@users.noreply.github.com>
2024-08-31 15:58:22 +0530

    css and images moved to `assets` folder

--- flierly-frontend/src/MainApp.jsx
+++ flierly-frontend/src/MainApp.jsx
@@ -1,10 +1,10 @@
-import '@/styles/app.css';
+import "@/assets/css/app.css";
 
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { lazy, Suspense } from 'react';
-import PageLoader from '@/components/PageLoader';
-import store from '@/redux/store';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { lazy, Suspense } from "react";
+import PageLoader from "@/components/PageLoader";
+import store from "@/redux/store";
 
 // Using lazy to dynamically import the Flierly component with a simulated delay of 3 seconds
 const Flierly = lazy(
@@ -17,9 +17,15 @@ const Flierly = lazy(
 // MainApp component that sets up routing, Redux store provider, and lazy loading for Flierly component
 function MainApp() {
   return (
-    <BrowserRouter> {/* Sets up the router context */}
-      <Provider store={store}> {/* Provides the Redux store to the app */}
-        <Suspense fallback={<PageLoader />}> {/* Suspense to show a loader while Flierly component is being loaded */}
+    <BrowserRouter>
+      {" "}
+      {/* Sets up the router context */}
+      <Provider store={store}>
+        {" "}
+        {/* Provides the Redux store to the app */}
+        <Suspense fallback={<PageLoader />}>
+          {" "}
+          {/* Suspense to show a loader while Flierly component is being loaded */}
           <Flierly /> {/* Lazy loaded Flierly App */}
         </Suspense>
       </Provider>
@@ -27,4 +33,4 @@ function MainApp() {
   );
 }
 
-export default MainApp
+export default MainApp;
