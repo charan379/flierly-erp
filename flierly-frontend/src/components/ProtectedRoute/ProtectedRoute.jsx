23bfad8d2296020a9d0cbc5ed523e11f64877191
charan379 <37008444+charan379@users.noreply.github.com>
2024-08-31 19:38:57 +0530

    minor tweaks

--- flierly-frontend/src/components/ProtectedRoute/ProtectedRoute.jsx
+++ flierly-frontend/src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,21 +1,30 @@
-import { useAuth } from '@/modules/auth/hooks/useAuth';
-import React from 'react';
-import { Navigate } from 'react-router-dom';
+import { useAuth } from "@/modules/auth/hooks/useAuth";
+import React from "react";
+import { Navigate } from "react-router-dom";
+import UnAuthorized from "../UnAuthorized";
 
 const ProtectedRoute = ({ element, requiredPermission }) => {
-    const { allowedAccess, isLoggedIn } = useAuth();
+  const { allowedAccess, isLoggedIn } = useAuth();
 
-    const callback = {
-        pathname: window.location.pathname,
-        search: window.location.search,
-        url: window.location.href
-    };
+  const callback = {
+    pathname: window.location.pathname,
+    search: window.location.search,
+    url: window.location.href,
+  };
 
-    if (!isLoggedIn || !allowedAccess.includes(requiredPermission)) {
-        return <Navigate to={`/unauthorized?callback=${encodeURIComponent(JSON.stringify(callback))}`} />;
-    }
+  if (!isLoggedIn) {
+    return (
+      <Navigate
+        to={`/login?callback=${encodeURIComponent(JSON.stringify(callback))}`}
+      />
+    );
+  }
 
-    return element;
+  if (!allowedAccess.includes(requiredPermission)) {
+    return <UnAuthorized />;
+  }
+
+  return element;
 };
 
 export default ProtectedRoute;
