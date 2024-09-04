d173aa22933ff1711a0517964722d9a15178ffd6
charan379 <37008444+charan379@users.noreply.github.com>
2024-09-04 23:00:56 +0530

    form added

---
+++ flierly-frontend/src/modules/iam/forms/QueryPrivilege/QueryPrivilege.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const QueryPrivilege = () => {
+  const { translate, langDirection } = useLocale(); // Using the custom hook to get translation function and language direction
+
+  return <div>QueryPrivilege</div>;
+};
+
+export default QueryPrivilege;
