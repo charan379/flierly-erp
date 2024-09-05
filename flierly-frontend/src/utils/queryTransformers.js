36e9c65f0d3cda49405364f3470585bdaa667b2a
charan379 <37008444+charan379@users.noreply.github.com>
2024-09-05 12:00:36 +0530

    query transformers

---
+++ flierly-frontend/src/utils/queryTransformers.js
@@ -0,0 +1,21 @@
+
+export default queryTransformers = {
+    textWithRegex: (value, namePath, allValues) => {
+        if (value)
+            return `/${value}/i`;
+        else
+            return null;
+    },
+    inArray: (value, namePath, allValues) => {
+        if (typeof value === "object" && value?.length > 0)
+            return { [namePath]: { $in: value } };
+        else
+            return null
+    },
+    dateRange: (value, namePath, allValues) => {
+        if (typeof value === "object" && value?.length > 0)
+            return { [namePath]: { $gte: new Date(value[0]).toISOString(), $lte: new Date(value[1]).toISOString() } }
+        else
+            return null
+    }
+}
