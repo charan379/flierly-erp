a733624cc318d5092cfe3c548e936690c94c0974
charan379 <37008444+charan379@users.noreply.github.com>
2024-09-06 23:37:56 +0530

    autoclose row menu

--- flierly-frontend/src/features/CrudTable/features/RowContextMenu/RowContextMenu.jsx
+++ flierly-frontend/src/features/CrudTable/features/RowContextMenu/RowContextMenu.jsx
@@ -1,25 +1,39 @@
-import { useEscapeKey } from "@/hooks/useEscapeKey";
-// import { useOnOutsideScroll } from "@/hooks/useOnOutsideScroll";
-import useLocale from "@/features/Language/hooks/useLocale";
-import {
-  CheckCircleOutlined,
-  StopOutlined,
-} from "@ant-design/icons";
-import { Button, Flex, Menu, Popover, Tooltip, Typography } from "antd";
-import React, { useCallback, useEffect, useMemo, useState } from "react";
-import crudService from "../../service/crud.service";
-import { useOnOutsideClick } from "@/hooks/useOnOutSideClick";
-import useTheme from "@/features/Theme/hooks/useTheme";
-import { faCircleXmark, faEye, faPenToSquare, faTrashCan, faTrashCanArrowUp, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { useState, useEffect, useMemo, useCallback } from "react";
+import { Button, Divider, Flex, Menu, Popover, Tooltip, Typography } from "antd";
+import { CheckCircleOutlined, StopOutlined } from "@ant-design/icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleXmark,
+  faEye,
+  faPenToSquare,
+  faTrashCan,
+  faTrashCanArrowUp,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
+import useTheme from "@/features/Theme/hooks/useTheme";
+import useLocale from "@/features/Language/hooks/useLocale";
+import { useEscapeKey } from "@/hooks/useEscapeKey";
+import { useOnOutsideClick } from "@/hooks/useOnOutSideClick";
+import crudService from "../../service/crud.service";
 
-const RowContextMenu = ({ entity, actions, record, recordTitleKey, open, position, close }) => {
-  if (!open) return;
+const RowContextMenu = ({
+  entity,
+  actions,
+  record,
+  recordTitleKey,
+  open,
+  position,
+  close,
+}) => {
+  if (!open) return null;
+
   const { theme } = useTheme();
   const { translate } = useLocale();
   const [popoverPosition, setPopoverPosition] = useState(position);
+  const [countdown, setCountdown] = useState(30); // Start with 10 seconds
 
-  const menuItemStyle = { fontSize: "12px" }
+  const menuItemStyle = { fontSize: "12px" };
+
   const items = useMemo(() => {
     const baseItems = [
       {
@@ -28,123 +42,129 @@ const RowContextMenu = ({ entity, actions, record, recordTitleKey, open, positio
         icon: <FontAwesomeIcon icon={faEye} style={menuItemStyle} />,
         style: { color: "#2196F3", ...menuItemStyle },
       },
-    ];
-
-    if (true) {
-      baseItems.push({
+      {
         label: translate("edit"),
         key: "edit",
         icon: <FontAwesomeIcon icon={faPenToSquare} style={menuItemStyle} />,
         style: { color: "#FF9800", ...menuItemStyle },
-      });
-    }
-
-    if (record?.isActive) {
-      baseItems.push({
-        label: translate("inactivate"),
-        key: "inactivate",
-        icon: <StopOutlined style={menuItemStyle} />,
-        style: { color: "#9E9E9E", ...menuItemStyle },
-      });
-    }
-
-    if (!record?.isActive) {
-      baseItems.push({
-        label: translate("activate"),
-        key: "activate",
-        icon: <CheckCircleOutlined style={menuItemStyle} />,
-        style: { color: "#4CAF50", ...menuItemStyle },
-      });
-    }
-
-    if (true) {
-      baseItems.push({
+      },
+      record?.isActive
+        ? {
+            label: translate("inactivate"),
+            key: "inactivate",
+            icon: <StopOutlined style={menuItemStyle} />,
+            style: { color: "#9E9E9E", ...menuItemStyle },
+          }
+        : {
+            label: translate("activate"),
+            key: "activate",
+            icon: <CheckCircleOutlined style={menuItemStyle} />,
+            style: { color: "#4CAF50", ...menuItemStyle },
+          },
+      {
         label: translate("delete"),
         key: "delete",
         icon: <FontAwesomeIcon icon={faTrashCan} style={menuItemStyle} />,
         danger: true,
-        style: { ...menuItemStyle }
-      });
-    }
-
-    if (true) {
-      baseItems.push({
+      },
+      {
         label: translate("restore"),
         key: "restore",
-        icon: <FontAwesomeIcon icon={faTrashCanArrowUp} style={menuItemStyle} />,
+        icon: (
+          <FontAwesomeIcon icon={faTrashCanArrowUp} style={menuItemStyle} />
+        ),
         style: { color: "#009688", ...menuItemStyle },
-      });
-    }
-
-    if (true) {
-      baseItems.push({
-        label: translate("close"),
+      },
+      {
+        label: `${translate("close")} (${translate("auto_close_in")} ${countdown}s)`,
         key: "close",
         icon: <FontAwesomeIcon icon={faCircleXmark} style={menuItemStyle} />,
         danger: true,
-        style: { ...menuItemStyle },
-      });
-    }
-
+      },
+    ];
     return baseItems;
-  }, [record]);
+  }, [record, translate]);
 
-  const onMenuItemClick = useCallback(async ({ key }) => {
-    let result = {};
-    switch (key) {
-      case "activate":
-        result = await crudService.activate({
-          entity: entity,
-          action: "activate",
-          docIds: [record?._id],
-        });
-        break;
-      case "inactivate":
-        result = await crudService.activate({
-          entity: entity,
-          action: "inactivate",
-          docIds: [record?._id],
-        });
-        break;
-      case "delete":
-        result = await crudService.delete({
-          entity: entity,
-          docIds: [record?._id],
-        });
-        break;
-      case "restore":
-        result = await crudService.restore({
-          entity: entity,
-          docIds: [record?._id],
-        });
-        break;
-      default:
-        result = {};
-        break;
-    }
+  const onMenuItemClick = useCallback(
+    async ({ key }) => {
+      let result = {};
+      switch (key) {
+        case "activate":
+          result = await crudService.activate({
+            entity,
+            action: "activate",
+            docIds: [record?._id],
+          });
+          break;
+        case "inactivate":
+          result = await crudService.activate({
+            entity,
+            action: "inactivate",
+            docIds: [record?._id],
+          });
+          break;
+        case "delete":
+          result = await crudService.delete({
+            entity,
+            docIds: [record?._id],
+          });
+          break;
+        case "restore":
+          result = await crudService.restore({
+            entity,
+            docIds: [record?._id],
+          });
+          break;
+          case "restore":
+            result = await crudService.restore({
+              entity,
+              docIds: [record?._id],
+            });
+            break;
+          case "close":
+            close();
+            break;
+        default:
+          result = {};
+          break;
+      }
 
-    if (result?.success) {
-      actions.reload();
-      close();
-    }
-  }, [entity, record, actions, close]);
-
-  useEscapeKey(close);
-
-  useOnOutsideClick(
-    "row-popover-menu",
-    useCallback(() => {
-      close();
-    }, [close])
+      if (result?.success) {
+        actions.reload();
+        close();
+      }
+    },
+    [entity, record, actions, close]
   );
 
-  // useOnOutsideScroll(
-  //   "row-popover-menu",
-  //   useCallback(() => {
-  //     close();
-  //   }, [close])
-  // );
+  // Use escape key to close the menu
+  useEscapeKey(close);
 
+  // Close the menu when clicking outside
+  useOnOutsideClick(
+    "row-popover-menu",
+    useCallback(() => close(), [close])
+  );
+
+  // Timer countdown logic
+  useEffect(() => {
+    if (open) {
+      const timer = setInterval(() => {
+        setCountdown((prev) => {
+          if (prev <= 1) {
+            close();
+            clearInterval(timer);
+            return 0;
+          }
+          return prev - 1;
+        });
+      }, 1000);
+
+      return () => clearInterval(timer);
+    }
+  }, [open, close]);
+
+  // Adjust position logic
   useEffect(() => {
     if (open) {
       const handlePositioning = () => {
@@ -172,11 +192,9 @@ const RowContextMenu = ({ entity, actions, record, recordTitleKey, open, positio
         }
       };
 
-      // Use requestAnimationFrame to ensure it runs after render
       requestAnimationFrame(handlePositioning);
-
-      // Optional: handle resize to adjust the position
       window.addEventListener("resize", handlePositioning);
+
       return () => window.removeEventListener("resize", handlePositioning);
     }
   }, [open, position]);
@@ -187,25 +205,31 @@ const RowContextMenu = ({ entity, actions, record, recordTitleKey, open, positio
       title={
         <Flex justify="space-between" align="center" wrap="nowrap" gap="large">
           <Typography.Text>
-            {record[recordTitleKey] ?? translate('row_menu')}
+            {record[recordTitleKey] ?? translate("row_menu")}
           </Typography.Text>
-          <Tooltip title={translate('close_menu')}>
-            <Button shape="default" danger icon={<FontAwesomeIcon icon={faXmark} size="2xl" />} onClick={close} />
+          <Tooltip title={translate("close_menu")}>
+            <Button
+              shape="default"
+              danger
+              icon={<FontAwesomeIcon icon={faXmark} size="2xl" />}
+              onClick={close}
+            />
           </Tooltip>
         </Flex>
       }
       open={open}
       autoAdjustOverflow
-      placement=""
       showArrow={false}
       zIndex={1}
       content={
-        <Menu
-          theme={theme}
-          items={items}
-          onClick={onMenuItemClick}
-          selectable={false}
-        />
+        <div>
+          <Menu
+            theme={theme}
+            items={items}
+            onClick={onMenuItemClick}
+            selectable={false}
+          />
+        </div>
       }
       overlayStyle={{
         position: "fixed",
