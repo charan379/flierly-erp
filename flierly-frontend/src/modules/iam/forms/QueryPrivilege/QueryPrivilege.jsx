c0322ca0504f5fe1c0a3438bc7475879bd3e63cd
charan379 <37008444+charan379@users.noreply.github.com>
2024-09-07 19:04:45 +0530

    minor tweaks

--- flierly-frontend/src/modules/iam/forms/QueryPrivilege/QueryPrivilege.jsx
+++ flierly-frontend/src/modules/iam/forms/QueryPrivilege/QueryPrivilege.jsx
@@ -1,95 +1,117 @@
 import useLocale from "@/features/Language/hooks/useLocale";
 import queryTransformers from "@/utils/queryTransformers";
-import { ProForm, ProFormDateRangePicker, ProFormItem, ProFormSelect, ProFormText } from "@ant-design/pro-components";
+import {
+  ProForm,
+  ProFormDateRangePicker,
+  ProFormItem,
+  ProFormSelect,
+  ProFormText,
+} from "@ant-design/pro-components";
 import { Select } from "antd";
 import React from "react";
 
 const QueryPrivilege = () => {
   const { translate, langDirection } = useLocale(); // Using the custom hook to get translation function and language direction
 
-  return <div style={{ direction: langDirection, width: "80%" }}>
-    <ProForm.Group>
-      {/* name */}
-      <ProFormText
-        name={"name"}
-        label={translate("name")}
-        rules={[{ type: "string" }]}
-        placeholder={translate("name")}
-        colProps={{ xl: 12 }}
-        transform={queryTransformers.textWithRegex}
-      />
-      {/* access  */}
-      <ProFormSelect
-        allowClear
-        mode="multiple"
-        label={translate("access")}
-        name="access"
-        rules={[{ type: "array" }]}
-        placeholder={translate("choose_access")}
-        colProps={{ xl: 12 }}
-        options={[
-          {
-            label: "Create", value: "Create"
-          },
-          {
-            label: "Read", value: "Read"
-          }, {
-            label: "Update", value: "Update"
-          }, {
-            label: "Delete", value: "Delete"
-          }, {
-            label: "Manage", value: "Manage"
-          }]}
-        transform={queryTransformers.inArray}
-      />
-    </ProForm.Group>
-    <ProFormItem
-      rules={[{ type: "array" }]}
-      name={"model"}
-      label={translate("model")}
-      colProps={{ xl: 12 }}
-      transform={queryTransformers.inArray}
-    >
-      <Select
-        placeholder={translate("choose_model")}
-        allowClear
-        mode="multiple"
-        options={[
-          {
-            label: "Uom", value: "Uom"
-          },
-          {
-            label: "TaxIdentity", value: "TaxIdentity"
-          }, {
-            label: "User", value: "User"
-          }, {
-            label: "Role", value: "Role"
-          }, {
-            label: "Customer", value: "Customer"
-          }]} />
-    </ProFormItem>
-    <ProFormText
-      name={"code"}
-      label={translate("code")}
-      rules={[{ type: "string" }]}
-      placeholder={translate("code")}
-      colProps={{ xl: 12 }}
-    />
-    <ProForm.Group>
-      <ProFormDateRangePicker
-        colProps={{ xl: 10 }}
-        name={"createdAt"}
-        label={translate("created_at")}
-        transform={queryTransformers.dateRange}
-      />
-      <ProFormDateRangePicker
-        colProps={{ xl: 10 }}
-        name={"updatedAt"}
-        label={translate("updated_at")}
-        transform={queryTransformers.dateRange}
-      />
-    </ProForm.Group>
-  </div >;
+  return (
+    <div style={{ direction: langDirection, width: "80%" }}>
+      <ProForm.Group>
+        {/* name */}
+        <ProFormText
+          name={"name"}
+          label={translate("name")}
+          rules={[{ type: "string" }]}
+          placeholder={translate("name")}
+          transform={queryTransformers.textWithRegex}
+        />
+        {/* access  */}
+        <ProFormSelect
+          allowClear
+          mode="multiple"
+          label={translate("access")}
+          name="access"
+          rules={[{ type: "array" }]}
+          placeholder={translate("choose_access")}
+          options={[
+            {
+              label: "Create",
+              value: "Create",
+            },
+            {
+              label: "Read",
+              value: "Read",
+            },
+            {
+              label: "Update",
+              value: "Update",
+            },
+            {
+              label: "Delete",
+              value: "Delete",
+            },
+            {
+              label: "Manage",
+              value: "Manage",
+            },
+          ]}
+          transform={queryTransformers.inArray}
+        />
+        <ProFormItem
+          rules={[{ type: "array" }]}
+          name={"model"}
+          style={{ width: "100%" }}
+          label={translate("model")}
+          transform={queryTransformers.inArray}
+        >
+          <Select
+            placeholder={translate("choose_model")}
+            allowClear
+            mode="multiple"
+            options={[
+              {
+                label: "Uom",
+                value: "Uom",
+              },
+              {
+                label: "TaxIdentity",
+                value: "TaxIdentity",
+              },
+              {
+                label: "User",
+                value: "User",
+              },
+              {
+                label: "Role",
+                value: "Role",
+              },
+              {
+                label: "Customer",
+                value: "Customer",
+              },
+            ]}
+          />
+        </ProFormItem>
+        <ProFormText
+          name={"code"}
+          label={translate("code")}
+          rules={[{ type: "string" }]}
+          placeholder={translate("code")}
+        />
+        <ProForm.Group>
+          <ProFormDateRangePicker
+            name={"createdAt"}
+            label={translate("created_at")}
+            transform={queryTransformers.dateRange}
+          />
+          <ProFormDateRangePicker
+            name={"updatedAt"}
+            label={translate("updated_at")}
+            transform={queryTransformers.dateRange}
+          />
+        </ProForm.Group>
+      </ProForm.Group>
+    </div>
+  );
 };
 
 export default QueryPrivilege;
