ebd617b92c4db894466c54f3d9046c9dc944c383
charan379 <37008444+charan379@users.noreply.github.com>
2024-09-08 10:37:06 +0530

    wrap under context provider

--- flierly-frontend/src/modules/iam/pages/Roles/Roles.jsx
+++ flierly-frontend/src/modules/iam/pages/Roles/Roles.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import IamLayout from "../../layout/IamLayout";
 import { CrudTable } from "@/features/CrudTable";
 import roleColumns from "../../config/roleColumns";
+import CrudTableContextProvider from "@/features/CrudTable/components/CrudTableContextProvider";
 
 export default function Roles() {
   return (
     <IamLayout>
-      <CrudTable
-        entity="role"
-        columns={roleColumns}
-        dataSource={[]}
-        tableKey={"role-table"}
-        rowKey="_id"
-      />
+      <CrudTableContextProvider>
+        <CrudTable
+          entity="role"
+          columns={roleColumns}
+          dataSource={[]}
+          tableKey={"role-table"}
+          rowKey="_id"
+        />
+      </CrudTableContextProvider>
     </IamLayout>
   );
 }
--- flierly-frontend/src/modules/iam/pages/Users/Users.jsx
+++ flierly-frontend/src/modules/iam/pages/Users/Users.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import IamLayout from "../../layout/IamLayout";
 import { CrudTable } from "@/features/CrudTable";
 import userColumns from "../../config/userColumns";
+import CrudTableContextProvider from "@/features/CrudTable/components/CrudTableContextProvider";
 
 export default function Users() {
   return (
     <IamLayout>
-      <CrudTable
-        entity="user"
-        columns={userColumns}
-        dataSource={[]}
-        tableKey={"user-table"}
-        rowKey="_id"
-      />
+      <CrudTableContextProvider>
+        <CrudTable
+          entity="user"
+          columns={userColumns}
+          dataSource={[]}
+          tableKey={"user-table"}
+          rowKey="_id"
+        />
+      </CrudTableContextProvider>
     </IamLayout>
   );
 }
