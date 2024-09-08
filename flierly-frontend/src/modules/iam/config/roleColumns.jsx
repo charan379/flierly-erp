88d543e2c64877e7bd2bd5caabd74e3b2ac893ab
charan379 <37008444+charan379@users.noreply.github.com>
2024-09-08 21:03:13 +0530

    added query config

--- flierly-frontend/src/modules/iam/config/roleColumns.jsx
+++ flierly-frontend/src/modules/iam/config/roleColumns.jsx
@@ -15,31 +15,84 @@ const roleColumns = [
     title: "Name",
     dataIndex: "name",
     copyable: false,
-    width: 20,
+    width: 10,
+    queryFormConfig: {
+      name: "name",
+      label: "name",
+      rules: [{ type: "string" }],
+      transformer: "textWithRegex",
+      order: 1,
+      input: {
+        type: "Text",
+      },
+    },
   },
   {
     title: "Code",
     dataIndex: "code",
     copyable: true,
     width: 10,
+    queryFormConfig: {
+      name: "code",
+      label: "code",
+      rules: [{ type: "string" }],
+      transformer: "trimTextValue",
+      order: 2,
+      input: {
+        type: "Text",
+      },
+    },
   },
   {
     title: "Description",
     dataIndex: "description",
     copyable: false,
     width: 20,
+    queryFormConfig: {
+      name: "description",
+      label: "description",
+      rules: [{ type: "string" }],
+      transformer: "textWithRegex",
+      order: 3,
+      input: {
+        type: "Text",
+      },
+    },
   },
   {
     title: "Created",
     dataIndex: "createdAt",
-    width: 20,
+    width: 10,
     valueType: "dateTime",
+    sorter: true,
+    defaultSortOrder: "descend",
+    queryFormConfig: {
+      name: "createdAt",
+      label: "created_at",
+      rules: [],
+      transformer: "dateRange",
+      order: 4,
+      input: {
+        type: "DateRange",
+      },
+    },
   },
   {
     title: "Updated",
     dataIndex: "updatedAt",
-    width: 20,
+    width: 10,
     valueType: "dateTime",
+    sorter: true,
+    queryFormConfig: {
+      name: "updatedAt",
+      label: "updated_at",
+      rules: [],
+      transformer: "dateRange",
+      order: 5,
+      input: {
+        type: "DateRange",
+      },
+    },
   },
 ];
 
