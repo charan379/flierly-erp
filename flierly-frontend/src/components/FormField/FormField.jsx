92bbe24e2fbf8781dcbcce6b5e2d3bc5f2b5efcd
charan379 <37008444+charan379@users.noreply.github.com>
2024-09-08 20:16:09 +0530

    dynamic form item/field creator

---
+++ flierly-frontend/src/components/FormField/FormField.jsx
@@ -0,0 +1,89 @@
+import useLocale from "@/features/Language/hooks/useLocale";
+import hasOwnProperty from "@/utils/hasOwnProperty";
+import queryTransformers from "@/utils/queryTransformers";
+import {
+  ProFormDateRangePicker,
+  ProFormSelect,
+  ProFormText,
+} from "@ant-design/pro-components";
+import React from "react";
+
+const FormField = ({ key, config }) => {
+  const { translate } = useLocale();
+  const { name, label, rules = [], input } = config;
+
+  // Get transformer if specified
+  const transformer = hasOwnProperty(config, "transformer")
+    ? queryTransformers[config.transformer]
+    : undefined;
+
+  // Ensure 'name' and 'label' are provided
+  if (!name || !label) {
+    throw new Error(
+      "Missing required 'name' or 'label' in form field config:",
+      config
+    );
+  }
+
+  const { type, select } = input;
+
+  // Get the corresponding form component
+  const FormComponent = FormItems[type];
+  if (!FormComponent) {
+    throw new Error(
+      `Unknown form input type '${type}' in form field config:`,
+      config
+    );
+  }
+
+  // Render the form component
+  return (
+    <FormComponent
+      key={key ?? name}
+      name={name}
+      label={translate(label)}
+      rules={rules}
+      {...(select || {})}
+      transformer={transformer}
+    />
+  );
+};
+
+export default FormField;
+
+const FormItems = {
+  Text: ({ name, label, rules = [], transformer }) => (
+    <ProFormText
+      name={name}
+      label={label}
+      rules={rules}
+      transform={transformer}
+    />
+  ),
+  Select: ({
+    name,
+    label,
+    rules = [],
+    mode = "single",
+    options,
+    transformer,
+  }) => (
+    <ProFormSelect
+      name={name}
+      label={label}
+      mode={mode}
+      rules={rules}
+      options={options}
+      allowClear
+      transform={transformer}
+    />
+  ),
+  DateRange: ({ name, label, rules = [], transformer }) => (
+    <ProFormDateRangePicker
+      name={name}
+      label={label}
+      rules={rules}
+      transform={transformer}
+    />
+  ),
+};
