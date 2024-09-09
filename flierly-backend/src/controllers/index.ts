de8b2d0bde9fdd7fd34bbbfdb39d5446c04ff2e4
charan379 <37008444+charan379@users.noreply.github.com>
2024-09-09 21:31:19 +0530

    ignore some custom controller dirs

--- flierly-backend/src/controllers/index.ts
+++ flierly-backend/src/controllers/index.ts
@@ -3,16 +3,19 @@ import { getModelsList } from "@/models";
 import { globSync } from "glob";
 import path from "path";
 
+const ignoreDirs = ['misc-controller'];
 
-export const customCotrollers = globSync(`${__dirname}/**/*-controller`).map(controllerDir => {
-    const splittedName = path.basename(controllerDir).split('-');
-    const customCotroller = {
-        name: splittedName.slice(0, splittedName.length - 1).join('-'),
-        controllerDir: splittedName.join("-")
-    }
-    // console.debug(customCotroller);
-    return customCotroller;
-});
+export const customCotrollers = globSync(`${__dirname}/**/*-controller`)
+    .filter(controllerDir => !ignoreDirs.includes(controllerDir.split(/[\/\\]/g).slice(-1)[0]))
+    .map(controllerDir => {
+        const splittedName = path.basename(controllerDir).split('-');
+        const customCotroller = {
+            name: splittedName.slice(0, splittedName.length - 1).join('-'),
+            controllerDir: splittedName.join("-")
+        }
+        // console.debug(customCotroller);
+        return customCotroller;
+    });
 
 const controllers = async () => {
     const modelList = await getModelsList();
@@ -34,7 +37,7 @@ const controllers = async () => {
     }
 
     // console.debug(controllers)
-    
+
     return controllers;
 };
 export default controllers;
