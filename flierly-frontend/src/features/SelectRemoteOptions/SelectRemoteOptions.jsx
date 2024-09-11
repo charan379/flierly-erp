2fa2f55cf273c88ca54547df235ff220bb7b82c7
charan379 <37008444+charan379@users.noreply.github.com>
2024-09-11 13:19:50 +0530

    bug fixes and load options on first render

--- flierly-frontend/src/features/SelectRemoteOptions/SelectRemoteOptions.jsx
+++ flierly-frontend/src/features/SelectRemoteOptions/SelectRemoteOptions.jsx
@@ -1,31 +1,24 @@
 import debounce from "@/utils/debounce";
 import { Empty, Flex, Select, Spin } from "antd";
-import React, { useMemo, useRef, useState } from "react";
-import CustomAntDSelectTag from "./components/CustomAntDSelectTag";
+import React, { useMemo, useRef, useState, useEffect } from "react";
 
 const SelectRemoteOptions = ({
   asyncOptionsFetcher,
   debounceTimeout = 500,
   ...props
 }) => {
-  const [fetching, setFetching] = useState(true);
+  const [fetching, setFetching] = useState(false);
   const [options, setOptions] = useState([]);
-
   const fetchRef = useRef(0);
 
   // Memoized debounce function
   const debounceFetcher = useMemo(() => {
-    // Debounced loadOptions function
     const loadOptions = (value) => {
       fetchRef.current += 1;
       const fetchId = fetchRef.current;
-      setOptions([]); // Clear previous options
       setFetching(true);
       asyncOptionsFetcher(value).then((newOptions) => {
-        if (fetchId !== fetchRef.current) {
-          // Skip if outdated fetch
-          return;
-        }
+        if (fetchId !== fetchRef.current) return;
         setOptions(newOptions);
         setFetching(false);
       });
@@ -33,32 +26,40 @@ const SelectRemoteOptions = ({
     return debounce(loadOptions, debounceTimeout);
   }, [asyncOptionsFetcher, debounceTimeout]);
 
+  // Initial fetch on component mount
+  useEffect(() => {
+    if (options.length <= 0) {
+      debounceFetcher("");
+    }
+  }, [debounceFetcher]);
+
   return (
     <Select
       allowClear
       filterOption={false}
-      showSearch={true}
+      showSearch
       onSearch={debounceFetcher}
-      onFocus={() => debounceFetcher("")}
-      options={options}
-      labelInValue
-      tagRender={(props) => {
-        console.log(props);
-        return <CustomAntDSelectTag title={props.label} onClose={props.onClose} />;
+      onFocus={() => {
+        if (options.length <= 0) {
+          debounceFetcher("");
+        }
       }}
-      notFoundContent={
-        fetching ? (
-          <Flex align="center" justify="center">
-            {" "}
-            <Spin size="small" />{" "}
-          </Flex>
-        ) : (
-          <Empty />
-        )
-      }
+      options={options}
+      loading={true}
+      dropdownRender={(menu) => {
+        return fetching ? <Loader /> : menu;
+      }}
+      notFoundContent={<Empty />}
       {...props}
     />
   );
 };
 
+const Loader = () => {
+  return (
+    <Flex align="center" justify="center">
+      <Spin size="small" />
+    </Flex>
+  );
+};
 export default SelectRemoteOptions;
