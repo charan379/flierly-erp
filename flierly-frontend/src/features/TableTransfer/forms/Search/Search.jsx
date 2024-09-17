17f4d52fb82e4058bc6bc826166680e76510101d
charan379 <37008444+charan379@users.noreply.github.com>
2024-09-18 01:06:25 +0530

    updated search form

--- flierly-frontend/src/features/TableTransfer/forms/Search/Search.jsx
+++ flierly-frontend/src/features/TableTransfer/forms/Search/Search.jsx
@@ -1,26 +1,33 @@
-import { ProForm, ProFormDependency, ProFormSelect, ProFormText } from "@ant-design/pro-components";
+import {
+  ProForm,
+  ProFormDependency,
+  ProFormSelect,
+  ProFormText,
+} from "@ant-design/pro-components";
 import { Space } from "antd";
 import React from "react";
 import FormField from "@/components/FormField";
 import hasOwnProperty from "@/utils/hasOwnProperty";
 import getColumnQueryConfig from "@/utils/getColumnQueryConfig";
 
-const Search = ({ columns }) => {
+const Search = ({ columns, onSearch }) => {
 
   const fieldFormItemOptions = columns
     .filter((column) => hasOwnProperty(column, "queryFormConfig"))
     .map((column) => {
-      return { label: column.title, value: column.dataIndex }
+      return { label: column.title, value: column.dataIndex };
     });
 
   return (
     <ProForm
       id="table-transfer-search-form"
       layout="inline"
-      onFinish={(values) => {
-        console.log(values)
-      }}>
-      <Space.Compact id="table-transfer-search-form-spc" style={{ maxWidth: "50%", width: "50%" }}>
+      onFinish={onSearch}
+    >
+      <Space.Compact
+        id="table-transfer-search-form-spc"
+        style={{ maxWidth: "50%", width: "50%" }}
+      >
         <ProFormSelect
           name={["field"]}
           rules={[{ required: true, message: "Search field is required" }]}
@@ -28,16 +35,27 @@ const Search = ({ columns }) => {
         />
         <ProFormDependency name={["field"]}>
           {({ field }) => {
-
             if (field === undefined) {
-              return (<ProFormText
-                name={["serach", "value"]}
-                rules={[{ required: true, message: "Search value is required" }]}
-              />)
+              return (
+                <ProFormText
+                  name={["serach", "value"]}
+                  rules={[
+                    { required: true, message: "Search value is required" },
+                  ]}
+                />
+              );
             }
 
             return (
-              <FormField showLabel={false} config={getColumnQueryConfig({ field, columns, required: true })} />)
+              <FormField
+                showLabel={false}
+                config={getColumnQueryConfig({
+                  field,
+                  columns,
+                  required: true,
+                })}
+              />
+            );
           }}
         </ProFormDependency>
       </Space.Compact>
