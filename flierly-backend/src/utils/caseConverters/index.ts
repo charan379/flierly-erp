70d0900867da8caf2b1e2d8a4a92acf994811bb1
charan379 <37008444+charan379@users.noreply.github.com>
2024-09-21 16:25:17 +0530

    added new converter `pascalToSnakeCase`

--- flierly-backend/src/utils/caseConverters/index.ts
+++ flierly-backend/src/utils/caseConverters/index.ts
@@ -1,5 +1,6 @@
 import kebabToPascalCase from './convertors/kebabToPascalCase';
 import camelToSnakeCase from './convertors/camelToSnakeCase';
 import camelToKebabCase from './convertors/camelToKebabCase';
+import pascalToSnakeCase from './convertors/pascalToSnakeCase';
 
-export { camelToSnakeCase, camelToKebabCase, kebabToPascalCase };
+export { camelToSnakeCase, camelToKebabCase, kebabToPascalCase, pascalToSnakeCase };
