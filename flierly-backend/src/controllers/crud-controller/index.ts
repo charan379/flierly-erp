fc6786ae81e47e5ab574118a6c0c76979b980681
charan379 <37008444+charan379@users.noreply.github.com>
2024-09-21 16:59:28 +0530

    added genric crud controller

---
+++ flierly-backend/src/controllers/crud-controller/index.ts
@@ -0,0 +1,35 @@
+import { EntityTarget, ObjectLiteral } from 'typeorm';
+import create from './create';
+import { Request, Response } from 'express';
+import read from './read';
+import update from './update';
+import softDelete from './delete';
+import page from './page';
+import search from './search';
+import exists from './exists';
+import activate from './activate';
+import restore from './restore';
+import deactivate from './deactivate';
+
+const CRUDController = async (entityDetails: EntityDetails) => {
+
+    const entity: EntityTarget<ObjectLiteral> = entityDetails.entity;
+
+    let crudMethods = {
+        create: (req: Request, res: Response) => create(entity, req, res),
+        read: (req: Request, res: Response) => read(entity, req, res),
+        update: (req: Request, res: Response) => update(entity, req, res),
+        delete: (req: Request, res: Response) => softDelete(entity, req, res),
+        page: (req: Request, res: Response) => page(entity, req, res),
+        search: (req: Request, res: Response) => search(entity, req, res),
+        exists: (req: Request, res: Response) => exists(entity, req, res),
+        activate: (req: Request, res: Response) => activate(entity, req, res),
+        deactivate: (req: Request, res: Response) => deactivate(entity, req, res),
+        restore: (req: Request, res: Response) => restore(entity, req, res),
+    }
+
+    return crudMethods;
+
+};
+
+export default CRUDController;
