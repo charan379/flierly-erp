5b353d672be409a721703c0ac91434358079ab86
charan379 <37008444+charan379@users.noreply.github.com>
2024-09-22 12:25:37 +0530

    generate controllers based on entities

--- flierly-backend/src/controllers/index.ts
+++ flierly-backend/src/controllers/index.ts
@@ -1,46 +1,44 @@
 import getEntityList from "@/entities";
-import CRUDController from "@/lib/crud-controller";
-import { getModelsList } from "@/models";
 import { globSync } from "glob";
 import path from "path";
+import CRUDController from "./crud-controller";
 
-const ignoreDirs = ['misc-controller', 'crud-controller', 'user-controller', 'branch-controller'];
+const ignoreDirs = ['misc-controller', 'crud-controller', 'branch-controller', 'user-controller'];
 
 export const customCotrollers = globSync(`${__dirname}/**/*-controller`)
     .filter(controllerDir => !ignoreDirs.includes(controllerDir.split(/[\/\\]/g).slice(-1)[0]))
     .map(controllerDir => {
+
         const splittedName = path.basename(controllerDir).split('-');
+
         const customCotroller = {
-            name: splittedName.slice(0, splittedName.length - 1).join('-'),
-            controllerDir: splittedName.join("-")
+            name: splittedName.join("-"),
+            directory: controllerDir
         }
-        // console.debug(customCotroller);
+
         return customCotroller;
     });
 
 const controllers = async () => {
-    const modelList = await getModelsList();
     const entityList = await getEntityList();
-    
+
     // Create an empty object to hold controllers before the loop
     const controllers: Record<string, object> = {};
 
-    const customControllerNames: string[] = customCotrollers.map((controller => controller.name));
+    const customControllersList: { name: string, directory: string }[] = customCotrollers;
 
     // Use a loop with `await` for each iteration
-    for (const model of modelList) {
-        // console.debug(model);
-        if (customControllerNames.includes(model.name)) {
-            const controller = await require(`@/controllers/${model.name}-controller`).default();
-            controllers[model.name] = controller;
+    for (const entityDetail of entityList) {
+        if (customControllersList.some((controller) => controller.name === entityDetail.controller)) {
+            const controller = await require(`@/controllers/${entityDetail.controller}`).default();
+            controllers[entityDetail.controller] = controller;
         }
         else {
-            controllers[model.name] = await CRUDController(model.entity);
+            controllers[entityDetail.controller] = await CRUDController(entityDetail);
         }
     }
 
-    // console.debug(controllers)
-
     return controllers;
 };
+
 export default controllers;
