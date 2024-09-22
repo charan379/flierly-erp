000e12bc964dcc4e45bc5011be453791bae0f253
charan379 <37008444+charan379@users.noreply.github.com>
2024-09-23 01:34:36 +0530

    custom controller

--- flierly-backend/src/controllers/user-controller/authenticate.ts
+++ flierly-backend/src/controllers/user-controller/authenticate.ts
@@ -8,7 +8,7 @@ import { UserPassword } from "@/models/interfaces/user-password.interface";
 import { User } from "@/models/interfaces/user.interface";
 import UserPasswordModel from "@/models/user/user-password.model";
 import UserModel from "@/models/user/user.model";
-import apiResponse from "@/utils/api-response.generator";
+import apiResponse from "@/utils/api/responseGenerator";
 import JoiSchemaValidator from "@/utils/joi-object-validator/joiSchemaValidator";
 import { Request, Response } from "express";
 import Joi from "joi";
@@ -20,52 +20,51 @@ export const credentialsSchema: Joi.ObjectSchema = Joi.object({
 });
 
 const authenticate = async (req: Request, res: Response) => {
-    // validate new user
-    const credentials: { username: string, password: string } = await JoiSchemaValidator(credentialsSchema, req.body, { abortEarly: false, allowUnknown: false }, "authenticate-user-controller");
-    // check if user exists with username
-    const user: User | null = await UserModel.findOne({ username: credentials.username }, { __v: 0 }, { autopopulate: true }
-    ).where('isDeleted', false).exec();
-    // Throw error if user does not exist
-    if (user === null)
-        throw new FlierlyException("Invalid username", HttpCodes.UNAUTHORIZED, "Can't find user with provided username", "authenticate-user-controller-invalid-username");
-    // validate credentials with password stored in database
-    const userPassword: UserPassword | null = await UserPasswordModel.findOne({ userId: user._id }, { __v: 0 }).exec();
-    // Throw exception if password not created for user
-    if (userPassword === null)
-        throw new FlierlyException("User not allowed to login !", HttpCodes.UNAUTHORIZED, "Password not generated for this user", "authenticate-user-controller-password-not-generated");
-    // validate hash
-    const isPasswordValid = await validateHash(credentials.password, userPassword.password);
-    // Throw error if provided password does not match with password stored in database
-    if (!isPasswordValid)
-        throw new FlierlyException("Invalid password", HttpCodes.BAD_REQUEST, "Password does not match", "authenticate-user-controller-invalid-password");
-    // Throw error if user is inactive
-    if (!user.isActive)
-        throw new FlierlyException("Inactive user", HttpCodes.BAD_REQUEST, "User is not activated", "authenticate-user-controller-inactive-user");
-    // generate jwt token for further authentication with username and userId
-    const token = await generateJwtToken(user._id, user.username);
-    // tokenExpiresAt
-    const tokenExpiresAt = new Date();
-    tokenExpiresAt.setHours(tokenExpiresAt.getHours() + 7, tokenExpiresAt.getMinutes() + 30);
-    // response object
-    const response = {
-        user,
-        allowedAccess: [...(await getUserPrivileges(user._id)).privilegeCodes],
-        token,
-        loggedInAt: new Date(),
-        tokenExpiresAt,
-    }
+    return res.status(HttpCodes.OK).json(apiResponse({
+        success: true,
+        controller: "user.authenticate",
+        error: null,
+        httpCode: HttpCodes.OK,
+        message: "Authentication route not implemented",
+        result: [],
+        req, res
+    }));
+    // // validate new user
+    // const credentials: { username: string, password: string } = await JoiSchemaValidator(credentialsSchema, req.body, { abortEarly: false, allowUnknown: false }, "authenticate-user-controller");
+    // // check if user exists with username
+    // const user: User | null = await UserModel.findOne({ username: credentials.username }, { __v: 0 }, { autopopulate: true }
+    // ).where('isDeleted', false).exec();
+    // // Throw error if user does not exist
+    // if (user === null)
+    //     throw new FlierlyException("Invalid username", HttpCodes.UNAUTHORIZED, "Can't find user with provided username", "authenticate-user-controller-invalid-username");
+    // // validate credentials with password stored in database
+    // const userPassword: UserPassword | null = await UserPasswordModel.findOne({ userId: user._id }, { __v: 0 }).exec();
+    // // Throw exception if password not created for user
+    // if (userPassword === null)
+    //     throw new FlierlyException("User not allowed to login !", HttpCodes.UNAUTHORIZED, "Password not generated for this user", "authenticate-user-controller-password-not-generated");
+    // // validate hash
+    // const isPasswordValid = await validateHash(credentials.password, userPassword.password);
+    // // Throw error if provided password does not match with password stored in database
+    // if (!isPasswordValid)
+    //     throw new FlierlyException("Invalid password", HttpCodes.BAD_REQUEST, "Password does not match", "authenticate-user-controller-invalid-password");
+    // // Throw error if user is inactive
+    // if (!user.isActive)
+    //     throw new FlierlyException("Inactive user", HttpCodes.BAD_REQUEST, "User is not activated", "authenticate-user-controller-inactive-user");
+    // // generate jwt token for further authentication with username and userId
+    // const token = await generateJwtToken(user._id, user.username);
+    // // tokenExpiresAt
+    // const tokenExpiresAt = new Date();
+    // tokenExpiresAt.setHours(tokenExpiresAt.getHours() + 7, tokenExpiresAt.getMinutes() + 30);
+    // // response object
+    // const response = {
+    //     user,
+    //     allowedAccess: [...(await getUserPrivileges(user._id)).privilegeCodes],
+    //     token,
+    //     loggedInAt: new Date(),
+    //     tokenExpiresAt,
+    // }
 
-    // responde with newly registered user details.
-    res.status(HttpCodes.OK).json(
-        apiResponse(
-            true,
-            response,
-            "User Successfully authenticated, and token generated.",
-            "user.authenticate",
-            req.url,
-            null,
-            HttpCodes.OK, req, res)
-    );
+    // // responde with newly registered user details.
 };
 
 export default authenticate;
--- flierly-backend/src/controllers/user-controller/refreshAccessToken.ts
+++ flierly-backend/src/controllers/user-controller/refreshAccessToken.ts
@@ -4,43 +4,42 @@ import FlierlyException from "@/lib/flierly.exception";
 import { generateJwtToken } from "@/lib/jwt";
 import { User } from "@/models/interfaces/user.interface";
 import UserModel from "@/models/user/user.model";
-import apiResponse from "@/utils/api-response.generator";
+import apiResponse from "@/utils/api/responseGenerator";
 import { Request, Response } from "express";
 
 const refreshAccessToken = async (req: Request, res: Response) => {
-    // check if user exists with userId
-    const user: User | null = await UserModel.findOne({ _id: req.userId }, { __v: 0 }, { autopopulate: true }
-    ).where('isDeleted', false).exec();
-    // Throw error if user does not exist
-    if (user === null)
-        throw new FlierlyException("Invalid userId", HttpCodes.UNAUTHORIZED, "Can't find user with provided username", "refresh-access-token-controller-invalid-userId");
-    // Throw error if user is inactive
-    if (!user.isActive)
-        throw new FlierlyException("Inactive user", HttpCodes.BAD_REQUEST, "User is not activated", "refresh-access-token-controller-inactive-user");
-    // generate jwt token for further authentication with username
-    const token = await generateJwtToken(user._id, user.username);
-    // tokenExpiresAt
-    const tokenExpiresAt = new Date();
-    tokenExpiresAt.setHours(tokenExpiresAt.getHours() + 7, tokenExpiresAt.getMinutes() + 30);
-    // response object
-    const response = {
-        user,
-        allowedAccess: [...(await getUserPrivileges(user._id)).privilegeCodes],
-        token,
-        tokenExpiresAt,
-    }
+    return res.status(HttpCodes.OK).json(apiResponse({
+        success: true,
+        controller: "user.refreshAccessToken",
+        error: null,
+        httpCode: HttpCodes.OK,
+        message: "Refresh access token route not implemented",
+        result: [],
+        req, res
+    }));
+    // // check if user exists with userId
+    // const user: User | null = await UserModel.findOne({ _id: req.userId }, { __v: 0 }, { autopopulate: true }
+    // ).where('isDeleted', false).exec();
+    // // Throw error if user does not exist
+    // if (user === null)
+    //     throw new FlierlyException("Invalid userId", HttpCodes.UNAUTHORIZED, "Can't find user with provided username", "refresh-access-token-controller-invalid-userId");
+    // // Throw error if user is inactive
+    // if (!user.isActive)
+    //     throw new FlierlyException("Inactive user", HttpCodes.BAD_REQUEST, "User is not activated", "refresh-access-token-controller-inactive-user");
+    // // generate jwt token for further authentication with username
+    // const token = await generateJwtToken(user._id, user.username);
+    // // tokenExpiresAt
+    // const tokenExpiresAt = new Date();
+    // tokenExpiresAt.setHours(tokenExpiresAt.getHours() + 7, tokenExpiresAt.getMinutes() + 30);
+    // // response object
+    // const response = {
+    //     user,
+    //     allowedAccess: [...(await getUserPrivileges(user._id)).privilegeCodes],
+    //     token,
+    //     tokenExpiresAt,
+    // }
 
-    // responde with newly registered user details.
-    res.status(HttpCodes.OK).json(
-        apiResponse(
-            true,
-            response,
-            "Access token successfully refreshed.",
-            "user.refresh-access-token",
-            req.url,
-            null,
-            HttpCodes.OK, req, res)
-    );
+    // // responde with newly registered user details.
 };
 
 export default refreshAccessToken;
