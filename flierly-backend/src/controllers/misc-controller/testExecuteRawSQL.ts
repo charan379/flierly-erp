e75fb979c7145711972124ed4d6de6547abf4d6b
charan379 <37008444+charan379@users.noreply.github.com>
2024-09-24 00:52:39 +0530

    path changed

--- flierly-backend/src/controllers/misc-controller/testExecuteRawSQL.ts
+++ flierly-backend/src/controllers/misc-controller/testExecuteRawSQL.ts
@@ -1,7 +1,7 @@
 import HttpCodes from "@/constants/httpCodes";
 import { Privilege } from "@/entities/iam/Privilege.entity";
 import { idSchema } from "@/joi-schemas/common.joi.schemas";
-import executeQueryFromFile from "@/lib/queries/queryExecutor";
+import executeQueryFromFile from "@/lib/sql-queries/queryExecutor";
 import apiResponse from "@/utils/api/responseGenerator";
 import JoiSchemaValidator from "@/utils/joi-object-validator/joiSchemaValidator";
 import { Request, Response } from "express";
@@ -11,7 +11,7 @@ const testExecuteRawSQL = async (req: Request, res: Response): Promise<Response>
     const id = await JoiSchemaValidator<number>(idSchema, req.params.id, { abortEarly: false, allowUnknown: false }, "misc-controller-get-testExecuteRawSQL");
 
     console.time("testExecuteRawSQL");
-    const result: Privilege[] = await executeQueryFromFile<Privilege[]>("./iam/userPrivileges.sql", [id]);
+    const result: Privilege[] = await executeQueryFromFile<Privilege[]>("./iam/raw-sql/userPrivileges.sql", [id]);
     console.timeEnd('testExecuteRawSQL')
     return res.status(HttpCodes.OK).json(apiResponse({
         success: true,
