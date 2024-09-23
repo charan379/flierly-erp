1e6206743a8b72e7c0879acf9502ce0570324485
charan379 <37008444+charan379@users.noreply.github.com>
2024-09-23 15:40:52 +0530

    updated controller method `page`

--- flierly-backend/src/controllers/crud-controller/page.ts
+++ flierly-backend/src/controllers/crud-controller/page.ts
@@ -4,20 +4,37 @@ import apiResponse from "@/utils/api/responseGenerator";
 import { pascalToSnakeCase } from "@/utils/case-converters";
 import handleFilter from "@/utils/handle-filter";
 import JoiSchemaValidator from "@/utils/joi-object-validator/joiSchemaValidator";
+import pageResponseBuilder from "@/utils/page-response.builder";
 import { Request, Response } from "express";
 import Joi from "joi";
 import { EntityTarget, ObjectLiteral } from "typeorm";
 
+
+interface PageRequestBody {
+    autopopulateIds: boolean;
+    binMode: boolean;
+    pagination: { page: number, limit: number };
+    sort: { [key: string]: 'ascend' | 'descend' }
+    filters: FilterObject
+}
+
 const pageQuerySchema: Joi.ObjectSchema = Joi.object({
-    filter: Joi.object().required(),
-    page: Joi.number().default(1),
-    limit: Joi.number().default(50),
-    binMode: Joi.bool().default(false),
+    autopopulateIds: Joi.boolean().default(false), // Default: false
+    binMode: Joi.boolean().default(false), // Default: false
+    pagination: Joi.object({
+        page: Joi.number().integer().min(1).default(1), // Default: 1
+        limit: Joi.number().integer().min(1).default(20), // Default: 20
+    }).default({ page: 1, limit: 20 }), // Default pagination object
+    sort: Joi.object().pattern(
+        Joi.string(), // Any key allowed
+        Joi.string().valid('ascend', 'descend') // Values: 'ascend' or 'descend'
+    ).default({ createdAt: 'ascend' }), // Default sort by 'createdAt' ascending
+    filters: Joi.object().default({}) // Default: empty filter object
 })
 
 const page = async (entity: EntityTarget<ObjectLiteral>, req: Request, res: Response): Promise<Response> => {
 
-    const { filter, limit, page, binMode } = await JoiSchemaValidator<{ filter: object, limit: number, page: number, binMode: boolean }>(pageQuerySchema, req.body, { abortEarly: false, allowUnknown: false }, "dynamic-page");
+    const { filters, pagination, autopopulateIds, binMode, sort }: PageRequestBody = await JoiSchemaValidator<PageRequestBody>(pageQuerySchema, req.body, { abortEarly: false, allowUnknown: false }, "dynamic-page");
 
     const repo = AppDataSource.getRepository(entity);
 
@@ -25,28 +42,28 @@ const page = async (entity: EntityTarget<ObjectLiteral>, req: Request, res: Resp
     const queryBuilder = repo.createQueryBuilder(pascalToSnakeCase(entity.toString()));
 
     // Apply filters to the query builder
-    handleFilter(queryBuilder, pascalToSnakeCase(entity.toString()), filter);
+    handleFilter(queryBuilder, pascalToSnakeCase(entity.toString()), filters);
 
     if (binMode) {
         queryBuilder.withDeleted();
         queryBuilder.andWhere('deletedAt IS NOT NULL');
     }
 
+    if (autopopulateIds) {
+        queryBuilder.loadAllRelationIds();
+    }
+
     // Apply pagination (skip and take for offset and limit)
-    queryBuilder.skip((page - 1) * limit).take(limit);
+    queryBuilder.skip((pagination.page - 1) * pagination.limit).take(pagination.limit);
 
     // Get the paginated and filtered rows
     const [results, total] = await queryBuilder.getManyAndCount();
 
+    const page: PageResult = await pageResponseBuilder(results, pagination.page, pagination.limit, total, sort);
+
     return res.status(HttpCodes.OK).json(apiResponse({
         success: true,
-        result: {
-            results,
-            total,
-            page,
-            pageSize: limit,
-            totalPages: Math.ceil(total / limit)
-        },
+        result: page,
         controller: "CRUD.PageController",
         message: "Page fetched successfully",
         httpCode: HttpCodes.OK,
