b02829063141d925bbc3bdff1d1f8553b8306a0e
charan379 <37008444+charan379@users.noreply.github.com>
2024-09-24 11:52:10 +0530

    error boundary updated

--- flierly-backend/src/middlewares/error-boundary.middleware.ts
+++ flierly-backend/src/middlewares/error-boundary.middleware.ts
@@ -1,12 +1,17 @@
 import HttpCodes from "@/constants/httpCodes";
-import apiResponse from "@/utils/api-response.generator";
+import apiResponse from "@/utils/api/responseGenerator";
 import errrorMessageGenerator from "@/utils/errror-message.generator";
 import { NextFunction, Request, Response } from "express";
 
-
-export function errorBoundary(fn: (req: Request, res: Response, next: NextFunction) =>
-    Promise<any>, modelName: string): (req: Request, res: Response, next: NextFunction) =>
-        Promise<void | Response> {
+/**
+ * Wraps an asynchronous route handler with error handling logic.
+ * 
+ * @param {Function} fn - The asynchronous route handler function.
+ * @param {string} entityCode - The entity code used for logging and error identification.
+ * @returns {Function} - A function that wraps the route handler with error handling.
+ */
+export function errorBoundary(fn: (req: Request, res: Response, next: NextFunction) => Promise<any>, entityCode: string): (req: Request, res: Response, next: NextFunction) =>
+    Promise<void | Response> {
     return async (req: Request, res: Response, next: NextFunction) => {
         try {
             await fn(req, res, next);
@@ -15,14 +20,49 @@ export function errorBoundary(fn: (req: Request, res: Response, next: NextFuncti
             switch (errorMsg.name) {
                 case 'ValidationError':
                     // ValidationError
-                    return res.status(HttpCodes.BAD_REQUEST).json(apiResponse(false, null, 'Required fields are not supplied', `${modelName}.${fn.name}`, req.url, errorMsg, HttpCodes.BAD_REQUEST, req, res));
+                    return res.status(HttpCodes.BAD_REQUEST).json(apiResponse({
+                        success: false,
+                        result: null,
+                        message: errorMsg.message,
+                        controller: `${entityCode}.${fn.name}`,
+                        error: errorMsg,
+                        httpCode: HttpCodes.BAD_REQUEST,
+                        req, res
+                    }));
                 case 'FlierlyException':
                     // FlierlyException
-                    return res.status(HttpCodes.BAD_REQUEST).json(apiResponse(false, null, errorMsg.message, `${modelName}.${fn.name}`, req.url, errorMsg, HttpCodes.BAD_REQUEST, req, res));
+                    return res.status(HttpCodes.BAD_REQUEST).json(apiResponse({
+                        success: false,
+                        result: null,
+                        message: errorMsg.message,
+                        controller: `${entityCode}.${fn.name}`,
+                        error: errorMsg,
+                        httpCode: HttpCodes.BAD_REQUEST,
+                        req, res
+                    }));
+                case 'QueryFailedError':
+                    // QueryFailedError
+                    return res.status(HttpCodes.BAD_REQUEST).json(apiResponse({
+                        success: false,
+                        result: null,
+                        message: errorMsg.message,
+                        controller: `${entityCode}.${fn.name}`,
+                        error: errorMsg,
+                        httpCode: HttpCodes.BAD_REQUEST,
+                        req, res
+                    }))
                 default:
                     // Server Error
                     const httpCode = errorMsg?.httpCode ?? HttpCodes.INTERNAL_SERVER_ERROR;
-                    return res.status(httpCode).json(apiResponse(false, null, errorMsg.message, `${modelName}.${fn.name}`, req.url, errorMsg, httpCode, req, res));
+                    return res.status(httpCode).json(apiResponse({
+                        success: false,
+                        result: null,
+                        message: errorMsg.message,
+                        controller: `${entityCode}.${fn.name}`,
+                        error: errorMsg,
+                        httpCode: HttpCodes.BAD_REQUEST,
+                        req, res
+                    }));
             }
         }
     };
