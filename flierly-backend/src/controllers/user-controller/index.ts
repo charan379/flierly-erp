a1cc49d44071b29c4dc0129e38ee465c4e112ed1
charan379 <37008444+charan379@users.noreply.github.com>
2024-09-24 15:57:56 +0530

    use string entity

--- flierly-backend/src/controllers/user-controller/index.ts
+++ flierly-backend/src/controllers/user-controller/index.ts
@@ -1,8 +1,7 @@
-import { User } from "@/entities/iam/User.entity";
 import CRUDController from "../crud-controller";
 
 const userController = async () => {
-    const defaultController = await CRUDController(User);
+    const defaultController = await CRUDController("User");
     // console.debug(defaultController)
     const controllers = {...defaultController};
     // console.debug(controllers);
