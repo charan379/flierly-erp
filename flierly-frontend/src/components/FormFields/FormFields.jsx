6e8f84e2d6ec318109111d8718ee87ae5fcc1e02
charan379 <37008444+charan379@users.noreply.github.com>
2024-09-26 16:24:43 +0530

    add sort and flex

--- flierly-frontend/src/components/FormFields/FormFields.jsx
+++ flierly-frontend/src/components/FormFields/FormFields.jsx
@@ -3,18 +3,19 @@ import React from "react";
 import { generate as uniqueId } from "shortid";
 import FormField from "@/components/FormField";
 import hasOwnProperty from "@/utils/hasOwnProperty";
+import { Flex } from "antd";
 
 const FormFields = ({ columns = [], configKey }) => {
   const { langDirection } = useLocale();
 
   return (
-    <div style={{ direction: langDirection }}>
-      {columns.map((column) => {
+    <Flex style={{ direction: langDirection }} gap="middle" wrap align="flex-start" justify="flex-start">
+      {columns.sort((c1, c2) => c1?.order - c2?.order).map((column) => {
         if (hasOwnProperty(column, configKey)) {
           return <FormField key={`${uniqueId()}`} config={column[configKey]} />;
         } else return;
       })}
-    </div>
+    </Flex>
   );
 };
 
