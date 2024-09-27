8b1a97fb599342a9b7f42766bf13aeef50a28944
charan379 <37008444+charan379@users.noreply.github.com>
2024-09-27 15:04:18 +0530

    code cleaning

--- flierly-frontend/src/components/FormField/FormField.jsx
+++ flierly-frontend/src/components/FormField/FormField.jsx
@@ -1,5 +1,6 @@
 import useLocale from "@/features/Language/hooks/useLocale";
 import SelectRemoteOptions from "@/features/SelectRemoteOptions";
+import { useAuth } from "@/modules/auth/hooks/useAuth";
 import hasOwnProperty from "@/utils/hasOwnProperty";
 import queryTransformers from "@/utils/queryTransformers";
 import {
@@ -11,9 +12,39 @@ import {
 import { Select } from "antd";
 import React from "react";
 
+// FormField component to render different form fields based on config
 const FormField = ({ key, config, showLabel = true }) => {
   const { translate } = useLocale();
-  const { name, label, rules = [], input, dependencies = [], hasFeedback = false, hidden = false, disabled = false, tooltip, width, allowClear = true } = config;
+  const { hasPermission } = useAuth();
+  const {
+    name, label, rules = [], input, dependencies = [], hasFeedback = false,
+    hidden = false, disabled = false, tooltip, width, allowClear = true,
+    access = { permission: "", ifNoAccess: "" }
+  } = config;
+
+  let isHidden = hidden;
+  let isDisabled = disabled;
+  let doNotRender = false;
+
+  const { permission, ifNoAccess } = access;
+
+  // Access check
+  if (permission && !hasPermission(permission)) {
+    switch (ifNoAccess) {
+      case "hide":
+        isHidden = true;
+        isDisabled = true;
+        break;
+      case "disable":
+        isDisabled = true;
+        break;
+      default:
+        doNotRender = true;
+        break;
+    }
+  }
+
+  if (doNotRender) return null;
 
   // Get transformer if specified
   const transformer = hasOwnProperty(config, "transformer")
@@ -22,21 +53,14 @@ const FormField = ({ key, config, showLabel = true }) => {
 
   // Ensure 'name' and 'label' are provided
   if (!name || !label) {
-    throw new Error(
-      "Missing required 'name' or 'label' in form field config:",
-      config
-    );
+    throw new Error(`Missing required 'name' or 'label' in form field config: ${JSON.stringify(config)}`);
   }
 
   const { type, select } = input;
+  const FormComponent = formItems[type];
 
-  // Get the corresponding form component
-  const FormComponent = FormItems[type];
   if (!FormComponent) {
-    throw new Error(
-      `Unknown form input type '${type}' in form field config:`,
-      config
-    );
+    throw new Error(`Unknown form input type '${type}' in form field config: ${JSON.stringify(config)}`);
   }
 
   // Render the form component
@@ -46,8 +70,8 @@ const FormField = ({ key, config, showLabel = true }) => {
       name={name}
       hasFeedback={hasFeedback}
       dependencies={dependencies}
-      hidden={hidden}
-      disabled={disabled}
+      hidden={isHidden}
+      disabled={isDisabled}
       tooltip={tooltip}
       width={width}
       label={showLabel ? translate(label) : null}
@@ -61,144 +85,64 @@ const FormField = ({ key, config, showLabel = true }) => {
 
 export default FormField;
 
-const FormItems = {
-  Text: ({ name, label, rules = [], transformer, hasFeedback = false, dependencies = [], hidden, disabled, tooltip, width, allowClear }) => {
-    return (
-      <ProFormText
-        name={name}
-        label={label}
-        rules={rules}
-        width={width}
-        hidden={hidden}
-        tooltip={tooltip}
-        disabled={hidden || disabled}
-        allowClear={allowClear}
-        transform={transformer}
-        hasFeedback={hasFeedback}
-        validateTrigge={['onChange']}
-        dependencies={dependencies}
+// Common ProForm component to handle shared props
+const ProFormComponent = ({ Component, ...props }) => (
+  <Component {...proFormProps(props)} />
+);
+
+// Function to extract common props for ProForm components
+const proFormProps = ({
+  name, label, rules = [], transformer, hasFeedback = false,
+  dependencies = [], hidden, disabled, tooltip, width, allowClear
+}) => ({
+  name,
+  label,
+  rules,
+  width,
+  hidden,
+  tooltip,
+  disabled: hidden || disabled,
+  allowClear,
+  transform: transformer,
+  hasFeedback,
+  validateTrigger: ['onChange'],
+  dependencies,
+});
+
+// Form items mapping
+const formItems = {
+  Text: (props) => <ProFormComponent {...props} Component={ProFormText} />,
+  Number: (props) => <ProFormComponent {...props} Component={ProFormDigit} />,
+  Select: (props) => (
+    <ProForm.Item {...proFormProps(props)} style={{ width: props.width ?? "100%" }}>
+      <Select
+        mode={props.mode || "single"}
+        placeholder="Please enter"
+        maxTagCount="responsive"
+        options={props.options}
+        allowClear={props.allowClear}
+        disabled={props.hidden || props.disabled}
+        hidden={props.hidden}
       />
-    )
-  },
-  Number: ({ name, label, rules = [], transformer, hasFeedback = false, dependencies = [], hidden, disabled, tooltip, width, allowClear }) => {
-    return (
-      <ProFormDigit
-        name={name}
-        label={label}
-        rules={rules}
-        width={width}
-        hidden={hidden}
-        tooltip={tooltip}
-        disabled={hidden || disabled}
-        allowClear={allowClear}
-        transform={transformer}
-        hasFeedback={hasFeedback}
-        validateTrigge={['onChange']}
-        dependencies={dependencies}
+    </ProForm.Item>
+  ),
+  DateRange: (props) => <ProFormComponent {...props} Component={ProFormDateRangePicker} />,
+  SelectRemoteOptions: (props) => (
+    <ProForm.Item {...proFormProps(props)} style={{ width: props.width ?? "100%" }}>
+      <SelectRemoteOptions
+        labelInValue={props.labelInValue}
+        mode={props.mode || "single"}
+        placeholder="Please enter"
+        asyncOptionsFetcher={props.asyncOptionsFetcher}
+        debounceTimeout={props.debounceTimeout}
+        labelRender={props.labelRender}
+        optionRender={props.optionRender}
+        tagRender={props.tagRender}
+        width={props.width}
+        allowClear={props.allowClear}
+        disabled={props.hidden || props.disabled}
+        hidden={props.hidden}
       />
-    )
-  },
-  Select: ({
-    name,
-    label,
-    rules = [],
-    mode = "single",
-    options,
-    transformer,
-    hasFeedback = false,
-    dependencies = [],
-    hidden, disabled, tooltip, width, allowClear
-  }) => {
-    return (
-      <ProForm.Item
-        name={name}
-        label={label}
-        rules={rules}
-        width={width}
-        hidden={hidden}
-        tooltip={tooltip}
-        disabled={hidden || disabled}
-        allowClear={allowClear}
-        style={{ width: width ?? "100%" }}
-        transform={transformer}
-        hasFeedback={hasFeedback}
-        dependencies={dependencies}
-      >
-        <Select
-          mode={mode}
-          placeholder={"Please enter"}
-          maxTagCount='responsive'
-          options={options}
-          allowClear={allowClear}
-        />
-      </ProForm.Item>
-    )
-  },
-  DateRange: ({ name, label, rules = [], transformer, hasFeedback = false, dependencies = [], hidden, disabled, tooltip, width, allowClear }) => {
-    return (
-      <ProFormDateRangePicker
-        name={name}
-        label={label}
-        rules={rules}
-        width={width}
-        hidden={hidden}
-        tooltip={tooltip}
-        disabled={hidden || disabled}
-        allowClear={allowClear}
-        transform={transformer}
-        hasFeedback={hasFeedback}
-        dependencies={dependencies}
-      />
-    )
-  },
-  SelectRemoteOptions: ({
-    name,
-    label,
-    rules = [],
-    mode = "single",
-    transformer,
-    debounceTimeout,
-    asyncOptionsFetcher,
-    labelRender,
-    optionRender,
-    tagRender,
-    labelInValue = false,
-    hasFeedback = false,
-    dependencies = [],
-    width,
-    hidden,
-    tooltip,
-    disabled,
-    allowClear,
-  }) => {
-    return (
-      <ProForm.Item
-        name={name}
-        label={label}
-        rules={rules}
-        style={{ width: "100%" }}
-        transform={transformer}
-        hasFeedback={hasFeedback}
-        dependencies={dependencies}
-        width={width}
-        hidden={hidden}
-        tooltip={tooltip}
-        disabled={hidden || disabled}
-        allowClear={allowClear}
-      >
-        <SelectRemoteOptions
-          labelInValue={labelInValue === true ? true : false}
-          mode={mode}
-          placeholder={"Please enter"}
-          asyncOptionsFetcher={asyncOptionsFetcher}
-          debounceTimeout={debounceTimeout}
-          labelRender={labelRender}
-          optionRender={optionRender}
-          tagRender={tagRender}
-          width={width}
-          allowClear={allowClear}
-        />
-      </ProForm.Item>
-    );
-  },
+    </ProForm.Item>
+  ),
 };
