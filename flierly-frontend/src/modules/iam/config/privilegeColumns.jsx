eb4bc533a2e5edab51d37153ee2b6cdde1dd5be8
charan379 <37008444+charan379@users.noreply.github.com>
2024-09-27 23:57:10 +0530

    reuse some config and added update from cofig

--- flierly-frontend/src/modules/iam/config/privilegeColumns.jsx
+++ flierly-frontend/src/modules/iam/config/privilegeColumns.jsx
@@ -3,6 +3,71 @@ import { Tag } from "antd";
 import entityExistenceValidator from "@/utils/validators/entityExistenceValidator";
 import timeStampColumns from "@/config/common/timeStampColumns";
 
+const commonColumnConfig = {
+  // active column
+  activeColumn: {
+    // input
+    input: {
+      options: [
+        { label: "Active", value: true },
+        { label: "In Active", value: false },
+      ],
+    },
+  },
+  // access column
+  accessColumn: {
+    // input
+    input: {
+      options: [
+        { label: "Create", value: "Create" },
+        { label: "Read", value: "Read" },
+        { label: "Update", value: "Update" },
+        { label: "Delete", value: "Delete" },
+        { label: "Manage", value: "Manage" },
+      ],
+    },
+  },
+  // entity column
+  entityColumn: {
+    // input
+    input: {
+      asyncOptionsFetcher: async (value) => {
+        const response = await selectRemoteOptionsService.entities({
+          keyword: value,
+        });
+        if (
+          response?.success &&
+          response?.result &&
+          Array.isArray(response.result)
+        ) {
+          return response.result.map((model) => {
+            return { label: model.entity, value: model.entity };
+          });
+        }
+      },
+    },
+  },
+  // code column
+  codeColumn: {
+    // validators
+    validators: {
+      // code regex pattern validator
+      codeRegex: ({}) => ({
+        validator(_, value) {
+          return new Promise((resolve, reject) => {
+            if (!value) resolve();
+            if (/^[a-z-]+\.[a-z]+$/.test(value)) {
+              resolve();
+            } else {
+              reject("code_is_not_valid");
+            }
+          });
+        },
+      }),
+    },
+  },
+};
+
 // Column configuration for "ID"
 const idColumn = {
   title: "ID",
@@ -11,56 +76,37 @@ const idColumn = {
   sorter: true,
   align: "center",
   order: 1,
-  // Uncomment and configure if needed
-  // createFormConfig: {
-  //   name: "id",
-  //   label: "id",
-  //   hidden: true,
-  //   disabled: true,
-  //   hasFeedback: false,
-  //   allowClear: false,
-  //   input: {
-  //     type: "Text",
-  //   },
-  //   rules: [
-  //     { type: "string", max: 10, required: false },
-  //     // Number regex validator
-  //     ({ }) => ({
-  //       validator(_, value) {
-  //         return new Promise((resolve, reject) => {
-  //           if (!value) resolve();
-  //           if (/^-?\d*(\.\d*)?$/.test(value)) {
-  //             resolve();
-  //           } else {
-  //             reject("id_is_not_a_valid_number");
-  //           }
-  //         });
-  //       }
-  //     }),
-  //     // ID already exists validator
-  //     ({ }) => ({
-  //       validator(_, value) {
-  //         if (value === undefined || value === "" || !/^-?\d*(\.\d*)?$/.test(value)) return Promise.resolve();
-  //         return entityExistenceValidator({
-  //           entity: "privilege",
-  //           filters: { id: value },
-  //           rejectionMessage: "id_already_exists"
-  //         });
-  //       },
-  //     }),
-  //   ],
-  // },
+  createFormConfig: {
+    name: "id",
+    label: "id",
+    hidden: true,
+    disabled: true,
+    hasFeedback: false,
+    allowClear: false,
+    input: {
+      type: "Text",
+    },
+  },
+  updateFormConfig: {
+    name: "id",
+    label: "id",
+    hidden: true,
+    disabled: true,
+    hasFeedback: false,
+    allowClear: false,
+    input: {
+      type: "Text",
+    },
+  },
   queryFormConfig: {
     name: "id",
     label: "id",
     allowClear: true,
-    width: 's',
+    width: "s",
     input: {
       type: "Number",
     },
-    rules: [
-      { type: "integer", required: false },
-    ],
+    rules: [{ type: "integer", required: false }],
   },
 };
 
@@ -77,15 +123,38 @@ const nameColumn = {
     label: "name",
     hasFeedback: true,
     allowClear: true,
-    access: { permission: /^privilege\.update$/, ifNoAccess: "disable" },
     rules: [
       { type: "string", required: true },
-      ({ }) => ({
+      ({}) => ({
         validator(_, value) {
           if (value === undefined) return Promise.resolve();
-          return entityExistenceValidator("privilege-name-validation-1", {
+          return entityExistenceValidator("privilege-name-validation-c-1", {
             entity: "privilege",
             filters: { name: { $ilike: value } },
+          });
+        },
+      }),
+    ],
+    input: {
+      type: "Text",
+    },
+  },
+  updateFormConfig: {
+    name: "name",
+    label: "name",
+    hasFeedback: true,
+    allowClear: true,
+    rules: [
+      { type: "string", required: true },
+      ({ getFieldValue }) => ({
+        validator(_, value) {
+          if (value === undefined) return Promise.resolve();
+          return entityExistenceValidator("privilege-name-validation-u-1", {
+            entity: "privilege",
+            filters: {
+              id: { $notEqualTo: getFieldValue("id") },
+              name: { $ilike: value },
+            },
           });
         },
       }),
@@ -130,10 +199,21 @@ const activeColumn = {
       type: "Select",
       select: {
         mode: "single",
-        options: [
-          { label: "Active", value: true },
-          { label: "In Active", value: false },
-        ],
+        options: commonColumnConfig.activeColumn.input.options,
+      },
+    },
+  },
+  updateFormConfig: {
+    name: "isActive",
+    label: "status",
+    access: { permission: /^privilege\.manage$/, ifNoAccess: "disable" },
+    allowClear: false,
+    rules: [],
+    input: {
+      type: "Select",
+      select: {
+        mode: "single",
+        options: commonColumnConfig.activeColumn.input.options,
       },
     },
   },
@@ -146,10 +226,7 @@ const activeColumn = {
       type: "Select",
       select: {
         mode: "single",
-        options: [
-          { label: "Active", value: true },
-          { label: "In Active", value: false },
-        ],
+        options: commonColumnConfig.activeColumn.input.options,
       },
     },
   },
@@ -184,18 +261,39 @@ const accessColumn = {
     allowClear: false,
     access: { permission: /^privilege\.update$/, ifNoAccess: "disable" },
     dependencies: ["entity"],
-    rules: [{ type: "enum", required: true, enum: ['Create', 'Read', "Update", "Delete", "Manage"] }],
+    rules: [
+      {
+        type: "enum",
+        required: true,
+        enum: ["Create", "Read", "Update", "Delete", "Manage"],
+      },
+    ],
     input: {
       type: "Select",
       select: {
         mode: "single",
-        options: [
-          { label: "Create", value: "Create" },
-          { label: "Read", value: "Read" },
-          { label: "Update", value: "Update" },
-          { label: "Delete", value: "Delete" },
-          { label: "Manage", value: "Manage" },
-        ],
+        options: commonColumnConfig.accessColumn.input.options,
+      },
+    },
+  },
+  updateFormConfig: {
+    name: "access",
+    label: "access",
+    allowClear: false,
+    access: { permission: /^privilege\.manage$/, ifNoAccess: "disable" },
+    dependencies: ["entity"],
+    rules: [
+      {
+        type: "enum",
+        required: true,
+        enum: ["Create", "Read", "Update", "Delete", "Manage"],
+      },
+    ],
+    input: {
+      type: "Select",
+      select: {
+        mode: "single",
+        options: commonColumnConfig.accessColumn.input.options,
       },
     },
   },
@@ -209,13 +307,7 @@ const accessColumn = {
       type: "Select",
       select: {
         mode: "multiple",
-        options: [
-          { label: "Create", value: "Create" },
-          { label: "Read", value: "Read" },
-          { label: "Update", value: "Update" },
-          { label: "Delete", value: "Delete" },
-          { label: "Manage", value: "Manage" },
-        ],
+        options: commonColumnConfig.accessColumn.input.options,
       },
     },
   },
@@ -241,11 +333,13 @@ const entityColumn = {
       // Entity Access Validator
       ({ getFieldValue }) => ({
         validator(_, value) {
-          if (value === undefined || !getFieldValue("access")) return Promise.resolve();
-          return entityExistenceValidator("privilege-entity-access-validation-1", {
+          if (value === undefined || !getFieldValue("access"))
+            return Promise.resolve();
+          return entityExistenceValidator("privilege-entity-access-val-c-1", {
             entity: "privilege",
             filters: { entity: value, access: getFieldValue("access") },
-            rejectionMessage: "privilege_with_entity_access_already_exists"
+            rejectionMessage:
+              "privilege_with_entity_same_access_already_exists",
           });
         },
       }),
@@ -254,20 +348,45 @@ const entityColumn = {
       type: "SelectRemoteOptions",
       select: {
         mode: "single",
-        asyncOptionsFetcher: async (value) => {
-          const response = await selectRemoteOptionsService.entities({
-            keyword: value,
+        asyncOptionsFetcher:
+          commonColumnConfig.entityColumn.input.asyncOptionsFetcher,
+        debounceTimeout: 300,
+      },
+    },
+  },
+  updateFormConfig: {
+    name: "entity",
+    label: "entity",
+    allowClear: false,
+    hasFeedback: true,
+    dependencies: ["access"],
+    access: { permission: /^privilege\.manage$/, ifNoAccess: "disable" },
+    rules: [
+      { required: true },
+      // Entity Access Validator
+      ({ getFieldValue }) => ({
+        validator(_, value) {
+          if (value === undefined || !getFieldValue("access"))
+            return Promise.resolve();
+          return entityExistenceValidator("privilege-entity-access-val-u-1", {
+            entity: "privilege",
+            filters: {
+              id: { $notEqualTo: getFieldValue("id") },
+              entity: value,
+              access: getFieldValue("access"),
+            },
+            rejectionMessage:
+              "privilege_with_entity_same_access_already_exists",
           });
-          if (
-            response?.success &&
-            response?.result &&
-            Array.isArray(response.result)
-          ) {
-            return response.result.map((model) => {
-              return { label: model.entity, value: model.entity };
-            });
-          }
         },
+      }),
+    ],
+    input: {
+      type: "SelectRemoteOptions",
+      select: {
+        mode: "single",
+        asyncOptionsFetcher:
+          commonColumnConfig.entityColumn.input.asyncOptionsFetcher,
         debounceTimeout: 300,
       },
     },
@@ -281,20 +400,8 @@ const entityColumn = {
       type: "SelectRemoteOptions",
       select: {
         mode: "multiple",
-        asyncOptionsFetcher: async (value) => {
-          const response = await selectRemoteOptionsService.entities({
-            keyword: value,
-          });
-          if (
-            response?.success &&
-            response?.result &&
-            Array.isArray(response.result)
-          ) {
-            return response.result.map((model) => {
-              return { label: model.entity, value: model.entity };
-            });
-          }
-        },
+        asyncOptionsFetcher:
+          commonColumnConfig.entityColumn.input.asyncOptionsFetcher,
         debounceTimeout: 300,
       },
     },
@@ -316,28 +423,41 @@ const codeColumn = {
     access: { permission: /^privilege\.update$/, ifNoAccess: "disable" },
     rules: [
       { type: "string", required: true },
-      ({ }) => ({
+      ({}) => ({
         validator(_, value) {
           if (value === undefined) return Promise.resolve();
           return entityExistenceValidator("privilege-code-validation-1", {
             entity: "privilege",
             filters: { code: value },
-            rejectionMessage: "privilege_with_same_code_already_exists"
+            rejectionMessage: "privilege_with_same_code_already_exists",
           });
         },
       }),
-      ({ }) => ({
+      commonColumnConfig.codeColumn.validators.codeRegex,
+    ],
+    input: {
+      type: "Text",
+    },
+  },
+  updateFormConfig: {
+    name: "code",
+    label: "code",
+    hasFeedback: true,
+    allowClear: true,
+    access: { permission: /^privilege\.manage$/, ifNoAccess: "disable" },
+    rules: [
+      { type: "string", required: true },
+      ({ getFieldValue }) => ({
         validator(_, value) {
-          return new Promise((resolve, reject) => {
-            if (!value) resolve();
-            if (/^[a-z]+\.[a-z]+$/.test(value)) {
-              resolve();
-            } else {
-              reject("code_is_not_valid");
-            }
+          if (value === undefined) return Promise.resolve();
+          return entityExistenceValidator("privilege-code-validation-1", {
+            entity: "privilege",
+            filters: { id: { $notEqualTo: getFieldValue("id") }, code: value },
+            rejectionMessage: "privilege_with_same_code_already_exists",
           });
         },
       }),
+      commonColumnConfig.codeColumn.validators.codeRegex,
     ],
     input: {
       type: "Text",
@@ -347,7 +467,10 @@ const codeColumn = {
     name: "code",
     label: "code",
     width: "s",
-    rules: [{ type: "string" }],
+    rules: [
+      { type: "string" },
+      commonColumnConfig.codeColumn.validators.codeRegex,
+    ],
     transformer: "trimTextValue",
     order: 4,
     input: {
@@ -356,6 +479,14 @@ const codeColumn = {
   },
 };
 
-const privilegeColumns = [idColumn, nameColumn, activeColumn, accessColumn, entityColumn, codeColumn, ...timeStampColumns];
+const privilegeColumns = [
+  idColumn,
+  nameColumn,
+  activeColumn,
+  accessColumn,
+  entityColumn,
+  codeColumn,
+  ...timeStampColumns,
+];
 
 export default privilegeColumns;
