9296080e3e8f0949cbefd34fd5f6d786c46007a7
charan379 <37008444+charan379@users.noreply.github.com>
2024-09-29 16:37:30 +0530

    update password

--- flierly-backend/src/routes/user-router/user.routes.ts
+++ flierly-backend/src/routes/user-router/user.routes.ts
@@ -1,5 +1,6 @@
 import authenticate from "@/controllers/user-controller/authenticate";
 import refreshAccessToken from "@/controllers/user-controller/refreshAccessToken";
+import updatePassword from "@/controllers/user-controller/updatePassword";
 import { authorize } from "@/middlewares/authorization.middleware";
 import { errorBoundary } from "@/middlewares/error-boundary.middleware";
 import { Router } from "express";
@@ -8,6 +9,7 @@ const userRouter = Router();
 
 userRouter.post(`/user/authenticate`, errorBoundary(authenticate, 'user'));
 userRouter.get(`/user/refresh-access-token`, authorize(), errorBoundary(refreshAccessToken, 'user'));
+userRouter.post(`user/update-password`, authorize("user.manage-password"), errorBoundary(updatePassword, 'user'));
 
 
 export default userRouter;
