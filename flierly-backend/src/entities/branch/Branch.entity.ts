2d77632c147ddcec9a4372ddef2ef54e11285908
charan379 <charanteja379@gmail.com>
2024-10-02 12:55:58 +0530

    validations updated

--- flierly-backend/src/entities/branch/Branch.entity.ts
+++ flierly-backend/src/entities/branch/Branch.entity.ts
@@ -1,6 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn, UpdateDateColumn, JoinColumn, OneToOne, DeleteDateColumn } from 'typeorm';
 import { Address } from '../address/Address.entity';
-import { IsEmail, IsNotEmpty, Length, Matches } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional, Length, Matches } from 'class-validator';
 import { TaxIdentity } from '../taxation/TaxIdentity.entity';
 
 @Entity('branches')
@@ -30,6 +30,7 @@ export class Branch {
 
     @Column({ nullable: true, name: 'alternate_phone' })
     @Matches(/^\+\d{1,3}[\s][6-9]\d{9}$/, { message: 'Alternate phone number must be in the format: +<country_code> <10-digit_number>' })
+    @IsOptional()
     alternatePhone: string;
 
     @OneToOne(() => Address, { eager: true })
