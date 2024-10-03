da0b99a1522a8abb1b76b91761645528c338c8dd
charan379 <37008444+charan379@users.noreply.github.com>
2024-10-03 14:29:22 +0530

    config updated

--- flierly-frontend/src/modules/branch/config/branchColumns.jsx
+++ flierly-frontend/src/modules/branch/config/branchColumns.jsx
@@ -1,17 +1,19 @@
 import { Tag } from "antd";
-import entityExistenceValidator from "@/utils/validators/entityExistenceValidator";
 import generateTimeStampColumns from "@/utils/column-generators/generateTimeStampColumns";
+import translate from "@/features/Language/utility/translate";
+import entityExistenceValidator from "@/utils/validators/entityExistenceValidator";
 
 // Regex patterns for validation
 const phoneRegex = /^\+\d{1,3}[\s][6-9]\d{9}$/;
 
 // Column configuration for "ID"
 const idColumn = {
-  title: "ID",
+  title: translate("id"),
   dataIndex: "id",
   width: 50,
   sorter: true,
   align: "center",
+  order: 1,
   createFormConfig: {
     name: "id",
     label: "ID",
@@ -34,21 +36,31 @@ const idColumn = {
       type: "Text",
     },
   },
+  queryFormConfig: {
+    name: "id",
+    label: "id",
+    allowClear: true,
+    width: "xs",
+    input: {
+      type: "Number",
+    },
+    rules: [{ type: "integer", required: false }],
+  },
 };
 
 // Column configuration for "Is Active"
 const isActiveColumn = {
-  title: "Active Status",
+  title: translate("active"),
   dataIndex: "isActive",
-  width: 120,
+  width: 100,
   sorter: true,
-  align: "center",
+  order: 2,
   render: (text) => {
     return text ? <Tag color="green">Active</Tag> : <Tag color="red">Inactive</Tag>;
   },
   createFormConfig: {
     name: "isActive",
-    label: "Active Status",
+    label: "active",
     allowClear: false,
     rules: [],
     input: {
@@ -57,9 +69,18 @@ const isActiveColumn = {
   },
   updateFormConfig: {
     name: "isActive",
-    label: "Active Status",
+    label: "active",
     allowClear: false,
     rules: [],
+    input: {
+      type: "Switch",
+    },
+  },
+  queryFormConfig: {
+    name: "isActive",
+    label: "active",
+    width: 120,
+    rules: [{ type: "boolean" }],
     input: {
       type: "Switch",
     },
@@ -68,26 +89,43 @@ const isActiveColumn = {
 
 // Column configuration for "Branch Name"
 const nameColumn = {
-  title: "Branch Name",
+  title: translate("branch_name"),
   dataIndex: "name",
   width: 150,
   sorter: true,
+  order: 3,
   createFormConfig: {
     name: "name",
-    label: "Branch Name",
+    label: "branch_name",
     hasFeedback: true,
     allowClear: true,
-    rules: [{ required: true, message: "Branch name is required." }, { min: 5, max: 90, message: 'Branch name must be between 5 and 90 characters.' }],
+    rules: [
+      { required: true, message: translate("branch_name_is_required") },
+      { min: 5, max: 90, message: translate("branch_name_must_be_between_5_and_90_characters") },
+    ],
     input: {
       type: "Text",
     },
   },
   updateFormConfig: {
     name: "name",
-    label: "Branch Name",
+    label: "branch_name",
     hasFeedback: true,
     allowClear: true,
-    rules: [{ required: true, message: "Branch name is required." }, { min: 5, max: 90, message: 'Branch name must be between 5 and 90 characters.' }],
+    rules: [
+      { required: true, message: translate("branch_name_is_required") },
+      { min: 5, max: 90, message: translate("branch_name_must_be_between_5_and_90_characters") },
+    ],
+    input: {
+      type: "Text",
+    },
+  },
+  queryFormConfig: {
+    name: "name",
+    label: "branch_name",
+    width: "s",
+    rules: [],
+    transformer: "ilike",
     input: {
       type: "Text",
     },
@@ -96,26 +134,62 @@ const nameColumn = {
 
 // Column configuration for "Email"
 const emailColumn = {
-  title: "Branch Email",
+  title: translate("email"),
   dataIndex: "email",
-  width: 150,
+  width: 180,
+  order: 4,
   sorter: true,
   createFormConfig: {
     name: "email",
-    label: "Branch Email",
+    label: "email",
     hasFeedback: true,
     allowClear: true,
-    rules: [{ required: true, message: "Branch email is required." }, { type: "email", message: "Invalid email format." }],
+    rules: [
+      { type: "email", required: true },
+      ({ }) => ({
+        validator(_, value) {
+          if (value === undefined) return Promise.resolve();
+          return entityExistenceValidator("branch-email-validation-c-1", {
+            entity: "branch",
+            filters: { email: { $ilike: value } },
+          });
+        },
+      }),
+    ],
     input: {
       type: "Text",
     },
   },
   updateFormConfig: {
     name: "email",
-    label: "Branch Email",
+    label: "email",
     hasFeedback: true,
     allowClear: true,
-    rules: [{ required: true, message: "Branch email is required." }, { type: "email", message: "Invalid email format." }],
+    rules: [
+      { type: "email", required: true },
+      ({ getFieldValue }) => ({
+        validator(_, value) {
+          if (value === undefined) return Promise.resolve();
+          return entityExistenceValidator("branch-email-validation-u-1", {
+            entity: "branch",
+            filters: {
+              id: { $notEqualTo: getFieldValue("id") },
+              email: { $ilike: value },
+            },
+          });
+        },
+      }),
+    ],
+    input: {
+      type: "Text",
+    },
+  },
+  queryFormConfig: {
+    name: "email",
+    label: "email",
+    width: "s",
+    rules: [{ type: "email" }],
+    transformer: "ilike",
     input: {
       type: "Text",
     },
@@ -124,17 +198,27 @@ const emailColumn = {
 
 // Column configuration for "Phone"
 const phoneColumn = {
-  title: "Branch Phone Number",
+  title: translate("phone_number"),
   dataIndex: "phone",
   width: 150,
   sorter: true,
+  order: 5,
   createFormConfig: {
     name: "phone",
-    label: "Branch Phone Number",
+    label: "phone_number",
     allowClear: true,
     rules: [
-      { required: true, message: "Branch phone number is required." },
-      { pattern: phoneRegex, message: 'Branch phone number must be in the format: +<country_code> <10-digit_number>' },
+      { required: true, message: translate("phone_is_required") },
+      { pattern: phoneRegex, message: translate("phone_is_not_valid") },
+      ({ }) => ({
+        validator(_, value) {
+          if (value === undefined) return Promise.resolve();
+          return entityExistenceValidator("barnch-phone-validation-c-1", {
+            entity: "branch",
+            filters: { registeredPhone: { $ilike: value } },
+          });
+        },
+      }),
     ],
     input: {
       type: "Text",
@@ -142,12 +226,34 @@ const phoneColumn = {
   },
   updateFormConfig: {
     name: "phone",
-    label: "Branch Phone Number",
+    label: "phone_number",
     allowClear: true,
     rules: [
-      { required: true, message: "Branch phone number is required." },
-      { pattern: phoneRegex, message: 'Branch phone number must be in the format: +<country_code> <10-digit_number>' },
+      { required: true, message: translate("phone_is_required") },
+      { pattern: phoneRegex, message: translate("phone_is_not_valid") },
+      ({ getFieldValue }) => ({
+        validator(_, value) {
+          if (value === undefined) return Promise.resolve();
+          return entityExistenceValidator("branch-phone-validation-u-1", {
+            entity: "branch",
+            filters: {
+              id: { $notEqualTo: getFieldValue("id") },
+              registeredPhone: { $ilike: value },
+            },
+          });
+        },
+      }),
     ],
+    input: {
+      type: "Text",
+    },
+  },
+  queryFormConfig: {
+    name: "phone",
+    label: "phone_number",
+    width: "s",
+    rules: [{ type: "string" }],
+    transformer: "ilike",
     input: {
       type: "Text",
     },
@@ -156,30 +262,37 @@ const phoneColumn = {
 
 // Column configuration for "Alternate Phone"
 const alternatePhoneColumn = {
-  title: "Alternate Phone Number",
+  title: translate("alternate_phone"),
   dataIndex: "alternatePhone",
   width: 180,
   sorter: true,
+  order: 6,
   createFormConfig: {
     name: "alternatePhone",
-    label: "Alternate Phone Number",
+    label: "alternate_phone",
+    hasFeedback: true,
     allowClear: true,
-    rules: [
-      { required: false },
-      { pattern: phoneRegex, message: 'Alternate phone number must be in the format: +<country_code> <10-digit_number>' },
-    ],
+    rules: [{ pattern: phoneRegex, message: translate("phone_is_not_valid") }],
     input: {
       type: "Text",
     },
   },
   updateFormConfig: {
     name: "alternatePhone",
-    label: "Alternate Phone Number",
+    label: "alternate_phone",
+    hasFeedback: true,
     allowClear: true,
-    rules: [
-      { required: false },
-      { pattern: phoneRegex, message: 'Alternate phone number must be in the format: +<country_code> <10-digit_number>' },
-    ],
+    rules: [{ pattern: phoneRegex, message: translate("phone_is_not_valid") }],
+    input: {
+      type: "Text",
+    },
+  },
+  queryFormConfig: {
+    name: "alternatePhone",
+    label: "alternate_phone",
+    width: "s",
+    rules: [{ type: "string" }],
+    transformer: "ilike",
     input: {
       type: "Text",
     },
@@ -188,14 +301,14 @@ const alternatePhoneColumn = {
 
 // Column configuration for "Address"
 const addressColumn = {
-  title: "Address",
+  title: translate("address"),
   dataIndex: "address",
   width: 150,
   sorter: true,
-  render: (address) => (address ? address.line1 : 'No Address'),
+  order: 7,
   createFormConfig: {
     name: "address",
-    label: "Address",
+    label: "address",
     hasFeedback: true,
     allowClear: true,
     rules: [],
@@ -206,7 +319,7 @@ const addressColumn = {
   },
   updateFormConfig: {
     name: "address",
-    label: "Address",
+    label: "address",
     hasFeedback: true,
     allowClear: true,
     rules: [],
@@ -219,14 +332,14 @@ const addressColumn = {
 
 // Column configuration for "Tax Identity"
 const taxIdentityColumn = {
-  title: "Tax Identity",
+  title: translate("tax_identity"),
   dataIndex: "taxIdentity",
   width: 150,
   sorter: true,
-  render: (taxIdentity) => (taxIdentity ? taxIdentity.name : 'No Tax Identity'),
+  order: 8,
   createFormConfig: {
     name: "taxIdentity",
-    label: "Tax Identity",
+    label: "tax_identity",
     hasFeedback: true,
     allowClear: true,
     rules: [],
@@ -237,7 +350,7 @@ const taxIdentityColumn = {
   },
   updateFormConfig: {
     name: "taxIdentity",
-    label: "Tax Identity",
+    label: "tax_identity",
     hasFeedback: true,
     allowClear: true,
     rules: [],
