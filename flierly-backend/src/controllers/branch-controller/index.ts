9b37d4b1d9aef0e5b8df7f6bd8c60646fd6ce439
charan379 <37008444+charan379@users.noreply.github.com>
2024-10-08 22:43:55 +0530

    bug fix

--- flierly-backend/src/controllers/branch-controller/index.ts
+++ flierly-backend/src/controllers/branch-controller/index.ts
@@ -1,8 +1,7 @@
-import { Branch } from "@/entities/branch/Branch.entity";
 import CRUDController from "../crud-controller";
 
 const branchController = async () => {
-    const defaultController = await CRUDController(Branch);
+    const defaultController = await CRUDController("Branch");
     // console.debug(defaultController)
     const controllers = { ...defaultController };
     // console.debug(controllers);
