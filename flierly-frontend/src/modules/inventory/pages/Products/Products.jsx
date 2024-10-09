81589ca24bf030b8ec37c6d4c3635785fc2851ea
charan379 <37008444+charan379@users.noreply.github.com>
2024-10-09 23:14:33 +0530

    added products page

--- flierly-frontend/src/modules/inventory/pages/Products/Products.jsx
+++ flierly-frontend/src/modules/inventory/pages/Products/Products.jsx
@@ -1,10 +1,26 @@
 import DefaultModuleLayout from "@/components/DefaultModuleLayout";
+import { CrudTable } from "@/features/CrudTable";
+import CrudTableContextProvider from "@/features/CrudTable/components/CrudTableContextProvider";
 import React from "react";
+import productColumns from "../../config/productColumns";
+import FormFields from "@/components/FormFields";
 
 const Products = () => {
   return (
     <DefaultModuleLayout>
-      <div>Products Page</div>
+      <CrudTableContextProvider>
+        <CrudTable
+          entity="product"
+          columns={productColumns}
+          dataSource={[]}
+          tableKey={"product-table"}
+          rowKey="id"
+          rowSelectionColumnWidth="30px"
+          searchFormFields={<FormFields columns={productColumns} configKey={"queryFormConfig"} />}
+          createFormFields={<FormFields columns={productColumns} configKey={"createFormConfig"} />}
+          updateFormFields={<FormFields columns={productColumns} configKey={"updateFormConfig"} />}
+        />
+      </CrudTableContextProvider>
     </DefaultModuleLayout>
   );
 };
