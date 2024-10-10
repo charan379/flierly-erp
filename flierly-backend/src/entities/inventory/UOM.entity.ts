1b19bba4f94a56f8304a57e1cc5f4dd57a8b4986
charan379 <37008444+charan379@users.noreply.github.com>
2024-10-10 11:22:34 +0530

    validations updated

--- flierly-backend/src/entities/inventory/UOM.entity.ts
+++ flierly-backend/src/entities/inventory/UOM.entity.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, Length } from 'class-validator';
+import { IsNotEmpty, Length, Matches } from 'class-validator';
 import {
   Entity,
   PrimaryGeneratedColumn,
@@ -21,14 +21,16 @@ export class UOM {
   @Column({ default: true, name: 'is_active' })
   isActive: boolean;
   
-  @Column({ type: 'varchar', length: 50, unique: true })
+  @Column({ type: 'varchar', length: 20, unique: true, nullable: false })
   @IsNotEmpty({ message: 'Short Name must not be empty.' })
-  @Length(1, 50, { message: 'Short Name must be between 1 and 50 characters.' })
+  @Length(1, 15, { message: 'Short Name must be between 1 and 15 characters.' })
+  @Matches(/^[a-zA-Z0-9]{1,15}$/, { message: "Short Name is not valid only letters, numbers allowed." })
   shortName: string;
 
   @Column({ type: 'varchar', length: 10, unique: true })
   @IsNotEmpty({ message: 'Code must not be empty.' })
-  @Length(2, 10, { message: 'Code must be between 2 and 10 characters.' })
+  @Length(1, 10, { message: 'Code must be between 1 and 10 characters.' })
+  @Matches(/^[a-z0-9]{1,10}$/, {message: "Code is not valid only letters and numbers allowed." })
   code: string;
 
   @CreateDateColumn({ name: 'created_at', type: 'timestamptz' })
