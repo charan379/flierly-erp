58ca6014c84702178cd94debfb957e80c278f504
charan379 <37008444+charan379@users.noreply.github.com>
2024-10-10 22:00:06 +0530

    stock page updated with table

--- flierly-frontend/src/modules/inventory/pages/Stocks/Stocks.jsx
+++ flierly-frontend/src/modules/inventory/pages/Stocks/Stocks.jsx
@@ -1,10 +1,26 @@
 import DefaultModuleLayout from "@/components/DefaultModuleLayout";
+import { CrudTable } from "@/features/CrudTable";
+import CrudTableContextProvider from "@/features/CrudTable/components/CrudTableContextProvider";
 import React from "react";
+import stockColumns from "../../config/stockColumns";
+import FormFields from "@/components/FormFields";
 
 const Stocks = () => {
   return (
     <DefaultModuleLayout>
-      <div>Stocks Page</div>
+      <CrudTableContextProvider>
+        <CrudTable
+          entity="stock"
+          columns={stockColumns}
+          dataSource={[]}
+          tableKey={"stock-table"}
+          rowKey="id"
+          rowSelectionColumnWidth="30px"
+          searchFormFields={<FormFields columns={stockColumns} configKey={"queryFormConfig"} />}
+          createFormFields={<FormFields columns={stockColumns} configKey={"createFormConfig"} />}
+          updateFormFields={<FormFields columns={stockColumns} configKey={"updateFormConfig"} />}
+        />
+      </CrudTableContextProvider>
     </DefaultModuleLayout>
   );
 };
