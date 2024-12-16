d8aac54fa5502869fc3190ffc07bf168b8646e47
charan379 <37008444+charan379@users.noreply.github.com>
2024-12-16 16:44:25 +0530

    updated routes

--- flierly-admin-web/src/modules/iam/router/IamRoutes.tsx
+++ flierly-admin-web/src/modules/iam/router/IamRoutes.tsx
@@ -1,127 +1,24 @@
-import PageLoader from "@/components/PageLoader";
-import WithSuspense from "@/components/WithSuspense";
 import ProtectedRoute from "@/features/ProtectedRoute/ProtectedRoute";
-import { QueryFieldConfig } from "@/features/QueryBuilder/QueryBuilder";
-import React, { Suspense } from "react";
+import PageUnderConstruction from "@/pages/PageUnderConstruction";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
-
-const QueryBuilder = React.lazy(() => import("@/features/QueryBuilder"));
-
-const exampleConfig: QueryFieldConfig<Record<string, any>>[] = [
-  {
-    field: { label: "Category", namePath: "category" },
-    conditions: [
-      {
-        condition: { label: "Equals To", namePath: "equalTo" },
-        formField: {
-          input: {
-            type: "Select",
-            options: [
-              { label: "Electronics", value: "electronics" },
-              { label: "Clothing", value: "clothing" },
-            ],
-          },
-        },
-      },
-      {
-        condition: { label: "Not Equals To", namePath: "notEqualTo" },
-        formField: {
-          input: {
-            type: "Select",
-            options: [
-              { label: "Electronics", value: "electronics" },
-              { label: "Clothing", value: "clothing" },
-            ],
-          },
-        },
-      },
-    ],
-  },
-  {
-    field: { label: "Price", namePath: "price" },
-    conditions: [
-      {
-        condition: { label: "Greater Than", namePath: "greaterThan" },
-        formField: { input: { type: "Number" } },
-      },
-      {
-        condition: { label: "Less Than", namePath: "lessThan" },
-        formField: { input: { type: "Number" } },
-      },
-      {
-        condition: { label: "Between", namePath: "between" },
-        formField: { input: { type: "NumberRange" } },
-      },
-    ],
-  },
-  {
-    field: { label: "Availability", namePath: "availability" },
-    conditions: [
-      {
-        condition: { label: "Equals", namePath: "equalTo" },
-        formField: {
-          input: {
-            type: "Select",
-            options: [
-              { label: "In Stock", value: "in_stock" },
-              { label: "Out of Stock", value: "out_of_stock" },
-            ],
-          },
-        },
-      },
-    ],
-  },
-];
+import Roles from "../pages/Roles";
+import Privileges from "../pages/Privileges";
 
 const IamRoutes: React.FC = () => {
   return (
     <Routes>
       {/* Default route */}
-      <Route
-        path=""
-        element={
-          <ProtectedRoute
-            // element={<WithSuspense importPath={import("@/pages/PageUnderConstruction")} />}
-            element={<Suspense fallback={<PageLoader />}>
-              <QueryBuilder config={exampleConfig} />
-            </Suspense>}
-            requiredPermissionRegex={/^user\.[a-z]+$/}
-          />
-        }
-      />
+      <Route path="" element={<ProtectedRoute element={<PageUnderConstruction />} requiredPermissionRegex={/^user\.[a-z]+$/} />} />
 
       {/* Users route */}
-      <Route
-        path="users"
-        element={
-          <ProtectedRoute
-            element={<WithSuspense importPath={import("@/pages/PageUnderConstruction")} />}
-            requiredPermissionRegex={/^user\.[a-z]+$/}
-          />
-        }
-      />
+      <Route path="users" element={<ProtectedRoute element={<PageUnderConstruction />} requiredPermissionRegex={/^user\.[a-z]+$/} />} />
 
       {/* Roles route */}
-      <Route
-        path="roles"
-        element={
-          <ProtectedRoute
-            element={<WithSuspense importPath={import("../pages/Roles")} />}
-            requiredPermissionRegex={/^role\.[a-z]+$/}
-          />
-        }
-      />
+      <Route path="roles" element={<ProtectedRoute element={<Roles />} requiredPermissionRegex={/^role\.[a-z]+$/} />} />
 
       {/* Privileges route */}
-      <Route
-        path="privileges"
-        element={
-          <ProtectedRoute
-            element={<WithSuspense importPath={import("../pages/Privileges")} />}
-            requiredPermissionRegex={/^privilege\.[a-z]+$/}
-          />
-        }
-      />
+      <Route path="privileges" element={<ProtectedRoute element={<Privileges />} requiredPermissionRegex={/^privilege\.[a-z]+$/} />} />
     </Routes>
   );
 };
--- flierly-admin-web/src/router/AppRouter.tsx
+++ flierly-admin-web/src/router/AppRouter.tsx
@@ -1,19 +1,20 @@
-import WithSuspense from "@/components/WithSuspense";
 import ProtectedRoute from "@/features/ProtectedRoute/ProtectedRoute";
 import Dashboard from "@/layout/Dashboard";
 import Login from "@/modules/auth/pages/Login";
 import SignUp from "@/modules/auth/pages/SignUp";
 import IamRoutes from "@/modules/iam/router/IamRoutes";
+import PageNotFound from "@/pages/PageNotFound";
+import PageUnderConstruction from "@/pages/PageUnderConstruction";
 import { Route, Routes } from "react-router-dom";
 
 const AppRouter = () => {
   return (
     <Routes>
       {/* Default route, page under construction */}
-      <Route path="/" element={<WithSuspense importPath={import("@/pages/PageUnderConstruction")} />} />
+      <Route path="/" element={<PageUnderConstruction />} />
 
       <Route path="/erp" element={<ProtectedRoute element={<Dashboard />} />}>
-        <Route path="" element={<WithSuspense importPath={import("@/pages/PageUnderConstruction")} />} />
+        <Route path="" element={<PageUnderConstruction />} />
         <Route path="iam/*" element={<IamRoutes />} />
       </Route>
       {/* Login and Register routes */}
@@ -21,7 +22,7 @@ const AppRouter = () => {
       <Route path="/register" element={<SignUp />} />
 
       {/* Catch-all for 404 */}
-      <Route path="*" element={<WithSuspense importPath={import("@/pages/PageNotFound")} />} />
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
 };
